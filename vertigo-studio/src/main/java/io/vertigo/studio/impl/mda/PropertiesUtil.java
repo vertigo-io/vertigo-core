a7fc117b59bf9c2649d6e4920b24998c62e35156
NPi2Loup <npiedeloup@gmail.com>
2014-07-25 09:52:53 +0200

    app builder

--- vertigo-studio/src/main/java/io/vertigo/studio/impl/mda/PropertiesUtil.java
+++ vertigo-studio/src/main/java/io/vertigo/studio/impl/mda/PropertiesUtil.java
@@ -18,15 +18,8 @@
  */
 package io.vertigo.studio.impl.mda;
 
-import io.vertigo.kernel.di.configurator.ComponentSpaceConfig;
-import io.vertigo.kernel.di.configurator.ComponentSpaceConfigBuilder;
 import io.vertigo.kernel.lang.Assertion;
-import io.vertigo.xml.XMLModulesLoader;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 /**
@@ -38,46 +31,6 @@ public final class PropertiesUtil {
 
 	private PropertiesUtil() {
 		// rien
-	}
-
-	/**
-	 * @param params Paramètres de l'application (environnement, webapp, ...)
-	 */
-	public static ComponentSpaceConfig parse(final Properties params, final boolean silence) {
-		final String[] xmlFileNames = params.getProperty("applicationConfiguration").split(";");
-		Assertion.checkArgument(xmlFileNames.length >= 1, "Il faut au moins une configuration");
-		//---------------------------------------------------------------------
-		final List<XMLModulesLoader> moduleLoaders = new ArrayList<>();
-		for (final String xmlFileName : xmlFileNames) {
-			final URL xmlUrl = PropertiesUtil.createURL(xmlFileName);
-			moduleLoaders.add(new XMLModulesLoader(xmlUrl, params));
-		}
-		final ComponentSpaceConfigBuilder componentSpaceConfigBuilder = new ComponentSpaceConfigBuilder() //
-				.withSilence(silence); //
-		//.withRestEngine(new GrizzlyRestEngine(8086));
-		for (final XMLModulesLoader modulesLoader : moduleLoaders) {
-			componentSpaceConfigBuilder.withLoader(modulesLoader);
-		}
-		return componentSpaceConfigBuilder.build();
-	}
-
-	/**
-	 * Retourne l'URL correspondant au nom du fichier dans le classPath.
-	 * 
-	 * @param fileName Nom du fichier
-	 * @return URN non null
-	 */
-	public static URL createURL(final String fileName) {
-		Assertion.checkArgNotEmpty(fileName);
-		//---------------------------------------------------------------------
-		try {
-			return new URL(fileName);
-		} catch (final MalformedURLException e) {
-			//Si fileName non trouvé, on recherche dans le classPath 
-			final URL url = PropertiesUtil.class.getResource(fileName);
-			Assertion.checkNotNull(url, "Impossible de récupérer le fichier [" + fileName + "]");
-			return url;
-		}
 	}
 
 	/**
