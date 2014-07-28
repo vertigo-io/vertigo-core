0c505a05b0d2a0c2800dc456dd141ab95d815dbc
NPi2Loup <npiedeloup@gmail.com>
2014-07-28 12:16:22 +0200

    License

--- vertigo-vega-api/src/main/java/io/vertigo/vega/rest/validation/AbstractDtObjectValidator.java
+++ vertigo-vega-api/src/main/java/io/vertigo/vega/rest/validation/AbstractDtObjectValidator.java
@@ -1,4 +1,5 @@
 /**
+ * vertigo - simple java starter
  *
  * Copyright (C) 2013, KleeGroup, direction.technique@kleegroup.com (http://www.kleegroup.com)
  * KleeGroup, Centre d'affaire la Boursidiere - BP 159 - 92357 Le Plessis Robinson Cedex - France
