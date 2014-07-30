33f6a43e6c4147a2e1c9690679e740c862bed82d
Jean-Michel Forhan <jmforhan@kleegroup.com>
2014-07-30 14:27:32 +0200

    Specify encoding for files generated by Studio

--- vertigo-studio/src/main/java/io/vertigo/studio/plugins/mda/AbstractConfiguration.java
+++ vertigo-studio/src/main/java/io/vertigo/studio/plugins/mda/AbstractConfiguration.java
@@ -25,53 +25,84 @@ import io.vertigo.studio.mda.Configuration;
 import java.util.Properties;
 
 /**
- * Configuration du generateur de fichiers. 
- * 
+ * Configuration du generateur de fichiers.
+ *
  * @author dchallas
  */
 public class AbstractConfiguration implements Configuration {
+
 	/**
 	 * Répertoire des fichiers générés une fois.
 	 * Doit être renseigné dans le fichier properties [targetDir]
 	 */
 	private final String targetDir;
-
 	/**
-	 * Répertoire des fichiers TOUJOURS générés 
+	 * Répertoire des fichiers TOUJOURS générés
 	 * Doit être renseigné dans le fichier properties [targetDir]
 	 */
 	private final String targetGenDir;
-
 	/**
 	 * Racine du projet.
 	 */
 	private final String projectPackageName;
+	/**
+	 * Encoding des fichiers générés.
+	 */
+	private final String encoding;
 
 	/**
 	 * Chargement des paramètres depuis le fichier properties.
+	 *
 	 * @param properties Paramètres de la génération
 	 */
 	protected AbstractConfiguration(final Properties properties) {
 		Assertion.checkNotNull(properties);
-		//---------------------------------------------------------------------
-		targetDir = getPropertyNotNull(properties, "targetDir", "Le repertoire des fichiers generes [targetDir] doit etre renseigne !");
+		// ---------------------------------------------------------------------
+		targetDir = getPropertyNotNull(properties, "targetDir",
+				"Le repertoire des fichiers generes [targetDir] doit etre renseigné !");
 		Assertion.checkState(targetDir.endsWith("/"), "Le chemin doit finir par '/'.");
-
-		targetGenDir = getPropertyNotNull(properties, "targetGenDir", "Le repertoire des fichiers generes [targetGenDir] doit etre renseigne !");
+		targetGenDir = getPropertyNotNull(properties, "targetGenDir",
+				"Le repertoire des fichiers generes [targetGenDir] doit etre renseigné !");
 		Assertion.checkState(targetGenDir.endsWith("/"), "Le chemin doit finir par '/'.");
-
-		projectPackageName = getPropertyNotNull(properties, "project.packagename", "le package racine du projet doit être renseigne ! [project.packagename]");
+		projectPackageName = getPropertyNotNull(properties, "project.packagename",
+				"le package racine du projet doit être renseigne ! [project.packagename]");
+		encoding = getPropertyNotNull(properties, "encoding",
+				"l'encoding des fichiers gérénés [encoding] doit etre renseigné !");
 	}
 
+	/**
+	 * Donne la valeur de projectPackageName.
+	 *
+	 * @return la valeur de projectPackageName.
+	 */
 	public final String getProjectPackageName() {
 		return projectPackageName;
 	}
 
+	/**
+	 * Donne la valeur de targetDir.
+	 *
+	 * @return la valeur de targetDir.
+	 */
 	final String getTargetDir() {
 		return targetDir;
 	}
 
+	/**
+	 * Donne la valeur de targetGenDir.
+	 *
+	 * @return la valeur de targetGenDir.
+	 */
 	final String getTargetGenDir() {
 		return targetGenDir;
+	}
+
+	/**
+	 * Donne la valeur de encoding.
+	 *
+	 * @return la valeur de encoding.
+	 */
+	final String getEncoding() {
+		return encoding;
 	}
 }
