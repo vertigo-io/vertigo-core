efc7f63a56119f865e8f9c0f7c8be4cc68b45e20
NPi2Loup <npiedeloup@gmail.com>
2014-08-01 14:48:58 +0200

    doc

--- vertigo-vega-impl/src/main/java/io/vertigo/vega/impl/rest/handler/RateLimitingHandler.java
+++ vertigo-vega-impl/src/main/java/io/vertigo/vega/impl/rest/handler/RateLimitingHandler.java
@@ -47,7 +47,7 @@ public final class RateLimitingHandler implements Activeable, RouteHandler {
 	private static final long DEFAULT_WINDOW_SECONDS = 15 * 60; //the time windows use to limit calls rate
 	private static final String RATE_LIMIT_LIMIT = "X-Rate-Limit-Limit"; //the rate limit ceiling for that given request
 	private static final String RATE_LIMIT_REMAINING = "X-Rate-Limit-Remaining"; //the number of requests left for the M minute window
-	private static final String RATE_LIMIT_RESET = "X-Rate-Limit-Reset"; //the remaining window before the rate limit resets in UTC epoch seconds
+	private static final String RATE_LIMIT_RESET = "X-Rate-Limit-Reset"; //the remaining seconds before the rate limit resets 
 
 	private final KSecurityManager securityManager;
 	private final long windowSeconds;
