308f37c65f0d069ee7ff7fdafbcca85cf7424a32
NPi2Loup <npiedeloup@gmail.com>
2014-11-04 18:53:38 +0100

    [Vega] tests

--- vertigo-vega-impl/src/test/java/io/vertigo/vega/rest/WsRestTest.java
+++ vertigo-vega-impl/src/test/java/io/vertigo/vega/rest/WsRestTest.java
@@ -81,6 +81,7 @@ import spark.Spark;
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.filter.session.SessionFilter;
 import com.jayway.restassured.parsing.Parser;
+import com.jayway.restassured.response.Response;
 import com.jayway.restassured.specification.RequestSpecification;
 import com.jayway.restassured.specification.ResponseSpecification;
 
@@ -271,7 +272,7 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 	public void testAuthentifiedTest() {
 		loggedAndExpect()
 				.statusCode(HttpStatus.SC_OK)
-				.body("size()", Matchers.equalTo(10))
+				.body("size()", Matchers.greaterThanOrEqualTo(10))
 				.when()
 				.get("/test/authentifiedTest");
 	}
@@ -473,21 +474,25 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 
 	@Test
 	public void testPostContact() throws ParseException {
-		final Map<String, Object> newContact = createContact(null, "Mrs", "Fournier", "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		doPostContact();
+	}
 
-		loggedAndExpect(given().body(newContact))
+	private Map<String, Object> doPostContact() throws ParseException {
+		final Map<String, Object> newContact = createDefaultContact(null);
+
+		final Long conId = loggedAndExpect(given().body(newContact))
+				.body("conId", Matchers.notNullValue())
 				.statusCode(HttpStatus.SC_OK)
 				.when()
-				.post("/test/contact");
+				.post("/test/contact")
+				.body().path("conId");
+		newContact.put("conId", conId);
+		return newContact;
 	}
 
 	@Test
 	public void testPostContactValidatorError() throws ParseException {
-		final Map<String, Object> newContact = createContact(100L, "Mrs", "Fournier", "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> newContact = createDefaultContact(100L);
 
 		loggedAndExpect(given().body(newContact))
 				.body("fieldErrors.conId", Matchers.contains("Id must not be set"))
@@ -495,9 +500,8 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 				.when()
 				.post("/test/contact");
 
-		final Map<String, Object> new2Contact = createContact(null, "Mrs", "Fournier", "Catherine", parseDate("24/10/2012"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> new2Contact = createDefaultContact(null);
+		new2Contact.put("birthday", convertDate("24/10/2012"));
 
 		loggedAndExpect(given().body(new2Contact))
 				.body("fieldErrors.birthday", Matchers.contains("You can't add contact younger than 16"))
@@ -508,9 +512,8 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 
 	@Test
 	public void testPostContactUserException() throws ParseException {
-		final Map<String, Object> newContact = createContact(null, "Mrs", null, "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> newContact = createDefaultContact(null);
+		newContact.remove("name");
 
 		loggedAndExpect(given().body(newContact))
 				.body("globalErrors", Matchers.contains("Name is mandatory"))
@@ -521,9 +524,7 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 
 	@Test
 	public void testPutContact() throws ParseException {
-		final Map<String, Object> newContact = createContact(100L, "Mrs", "Fournier", "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> newContact = createDefaultContact(100L);
 
 		loggedAndExpect(given().body(newContact))
 				.body("conId", Matchers.equalTo(100))
@@ -539,9 +540,7 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 
 	@Test
 	public void testPutContactByPath() throws ParseException {
-		final Map<String, Object> newContact = createContact(null, "Mrs", "Fournier", "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> newContact = createDefaultContact(null);
 
 		loggedAndExpect(given().body(newContact))
 				.body("conId", Matchers.equalTo(101))
@@ -557,9 +556,7 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 
 	@Test
 	public void testPutContactValidatorError() throws ParseException {
-		final Map<String, Object> newContact = createContact(null, "Mrs", "Fournier", "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> newContact = createDefaultContact(null);
 
 		loggedAndExpect(given().body(newContact))
 				.body("fieldErrors.conId", Matchers.contains("Id is mandatory"))
@@ -567,9 +564,8 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 				.when()
 				.put("/test/contact");
 
-		final Map<String, Object> new2Contact = createContact(100L, "Mrs", "Fournier", "Catherine", parseDate("24/10/2012"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> new2Contact = createDefaultContact(100L);
+		new2Contact.put("birthday", convertDate("24/10/2012"));
 
 		loggedAndExpect(given().body(new2Contact))
 				.body("fieldErrors.birthday", Matchers.contains("You can't add contact younger than 16"))
@@ -580,9 +576,8 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 
 	@Test
 	public void testPutContactByPathValidatorError() throws ParseException {
-		final Map<String, Object> new2Contact = createContact(null, "Mrs", "Fournier", "Catherine", parseDate("24/10/2012"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> new2Contact = createDefaultContact(null);
+		new2Contact.put("birthday", convertDate("24/10/2012"));
 
 		loggedAndExpect(given().body(new2Contact))
 				.body("fieldErrors.birthday", Matchers.contains("You can't add contact younger than 16"))
@@ -593,9 +588,8 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 
 	@Test
 	public void testPutContactUserException() throws ParseException {
-		final Map<String, Object> newContact = createContact(100L, "Mrs", null, "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> newContact = createDefaultContact(100L);
+		newContact.remove("name");
 
 		loggedAndExpect(given().body(newContact))
 				.body("globalErrors", Matchers.contains("Name is mandatory"))
@@ -606,9 +600,8 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 
 	@Test
 	public void testPutContactByPathUserException() throws ParseException {
-		final Map<String, Object> newContact = createContact(null, "Mrs", null, "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
-				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		final Map<String, Object> newContact = createDefaultContact(null);
+		newContact.remove("name");
 
 		loggedAndExpect(given().body(newContact))
 				.body("globalErrors", Matchers.contains("Name is mandatory"))
@@ -616,6 +609,86 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 				.when()
 				.put("/test/contact/101");
 	}
+
+	@Test
+	public void testFilteredUpdateByExclude() throws ParseException {
+		final Map<String, Object> contact = doGetServerSideObject();
+
+		final Long oldConId = (Long) contact.get("conId");
+		final String oldName = (String) contact.get("name");
+		final String oldFirstName = (String) contact.get("firstName");
+		final String oldEmail = (String) contact.get("email");
+		final String newFirstName = oldFirstName + "FNTest";
+		final String newEmail = "ETest." + oldEmail;
+
+		contact.remove("conId"); //can't modify conId
+		contact.remove("name"); //can't modify name
+		//contact.put("conId", 1000L);
+		//contact.put("name", newName);
+		contact.put("firstName", newFirstName);
+		contact.put("email", newEmail);
+
+		loggedAndExpect(given().body(contact))
+				.body("conId", Matchers.equalTo(oldConId))//not changed
+				.body("honorificCode", Matchers.equalTo(contact.get("honorificCode")))
+				.body("name", Matchers.equalTo(oldName)) //not changed
+				.body("firstName", Matchers.equalTo(newFirstName))
+				.body("birthday", Matchers.equalTo(contact.get("birthday")))
+				.body("email", Matchers.equalTo(newEmail))
+				.statusCode(HttpStatus.SC_OK)
+				.when()
+				.put("/test/filtered/" + oldConId);
+	}
+
+	@Test
+	public void testFilteredUpdateByExcludeErrors() throws ParseException {
+		final Map<String, Object> contact = doGetServerSideObject();
+		final Long oldConId = (Long) contact.get("conId");
+
+		contact.put("conId", 1000L);//can't modify conId
+		contact.put("name", "test"); //can't modify name
+		loggedAndExpect(given().body(contact))
+				.statusCode(HttpStatus.SC_FORBIDDEN)
+				.when()
+				.put("/test/filtered/" + oldConId);
+
+		contact.put("conId", 1000L);//can't modify conId
+		contact.remove("name"); //can't modify name
+		loggedAndExpect(given().body(contact))
+				.statusCode(HttpStatus.SC_FORBIDDEN)
+				.when()
+				.put("/test/filtered/" + oldConId);
+
+		contact.remove("conId");//can't modify conId
+		contact.put("name", "test"); //can't modify name
+		loggedAndExpect(given().body(contact))
+				.statusCode(HttpStatus.SC_FORBIDDEN)
+				.when()
+				.put("/test/filtered/" + oldConId);
+	}
+
+	@Test
+	public void testFilteredUpdateServerTokenErrors() throws ParseException {
+		final Map<String, Object> contact = doGetServerSideObject();
+		final Long oldConId = (Long) contact.get("conId");
+
+		contact.remove("conId");//can't modify conId
+		contact.remove("name"); //can't modify name
+
+		contact.put("serverToken", "badToken");
+		loggedAndExpect(given().body(contact))
+				.statusCode(HttpStatus.SC_FORBIDDEN)
+				.when()
+				.put("/test/filtered/" + oldConId);
+
+		contact.remove("serverToken");
+		loggedAndExpect(given().body(contact))
+				.statusCode(HttpStatus.SC_FORBIDDEN)
+				.when()
+				.put("/test/filtered/" + oldConId);
+	}
+
+	//=========================================================================
 
 	private ResponseSpecification expect() {
 		return RestAssured.expect();
@@ -637,11 +710,32 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 				.expect();
 	}
 
-	private Date parseDate(final String dateStr) throws ParseException {
-		return new SimpleDateFormat("dd/MM/yyyy").parse(dateStr);
+	private Map<String, Object> doGetServerSideObject() throws ParseException {
+		final Map<String, Object> contact = doPostContact();
+		final Long oldConId = (Long) contact.get("conId");
+
+		final Response getResponse = loggedAndExpect()
+				.statusCode(HttpStatus.SC_OK)
+				.when()
+				.get("/test/filtered/" + oldConId);
+		final String serverToken = getResponse.body().path("serverToken");
+		contact.put("serverToken", serverToken);
+		return contact;
 	}
 
-	private Map<String, Object> createContact(final Long conId, final String honorific, final String name, final String firstName, final Date birthday, final Map<String, Object> address, final String email, final String... tels) {
+	private String convertDate(final String dateStr) throws ParseException {
+		final Date date = new SimpleDateFormat("dd/MM/yyyy").parse(dateStr);
+		return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(date);
+	}
+
+	private Map<String, Object> createDefaultContact(final Long conId) throws ParseException {
+		final Map<String, Object> newContact = createContact2(conId, "Mrs", "Fournier", "Catherine", convertDate("24/10/1985"), //
+				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
+				"catherine.fournier@gmail.com", "01 91 92 93 94");
+		return newContact;
+	}
+
+	private Map<String, Object> createContact2(final Long conId, final String honorific, final String name, final String firstName, final String birthday, final Map<String, Object> address, final String email, final String... tels) {
 		final Map<String, Object> contact = new HashMap<>();
 		if (conId != null) {
 			contact.put("conId", conId);
@@ -649,7 +743,7 @@ public final class WsRestTest extends AbstractTestCaseJU4 {
 		contact.put("honorificCode", honorific);
 		contact.put("name", name);
 		contact.put("firstName", firstName);
-		contact.put("birthday", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").format(birthday));
+		contact.put("birthday", birthday);
 		contact.put("address", address);
 		contact.put("email", email);
 		contact.put("tels", Arrays.asList(tels));
