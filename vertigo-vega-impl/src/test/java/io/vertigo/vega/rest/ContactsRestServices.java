626c52938c30282170ce5be7254f704a7f570ede
phlppchrtn <pchretien55@gmail.com>
2014-11-13 22:20:33 +0100

    [vega]clean comments

--- vertigo-vega-impl/src/test/java/io/vertigo/vega/rest/ContactsRestServices.java
+++ vertigo-vega-impl/src/test/java/io/vertigo/vega/rest/ContactsRestServices.java
@@ -50,25 +50,25 @@ public final class ContactsRestServices implements RestfulService {
 	private KSecurityManager securityManager;
 
 	/*private final enum Group {
-		Friends("FRD", "Friends"), //
-		Familly("FAM", "Familly"), //
+		Friends("FRD", "Friends"),
+		Familly("FAM", "Familly"), 
 		CoWorkers("CWO", "Colleagues"), Familiar("FAR", "Familiar"),
 	}*/
 
 	public static enum Honorific {
-		Mr("MR_", "Mr", "Mister"), //
-		Miss("MIS", "Miss", "Miss"), //
-		Mrs("MRS", "Mrs", "Mrs"), //
-		Ms("MS_", "Ms.", "Ms."), //
-		Dr("DR_", "Dr.", "Doctor"), //
-		Cpt("CAP", "Cpt", "Captain"), //
-		Cch("CCH", "Cch", "Coach"), //
+		Mr("MR_", "Mr", "Mister"),
+		Miss("MIS", "Miss", "Miss"),
+		Mrs("MRS", "Mrs", "Mrs"),
+		Ms("MS_", "Ms.", "Ms."),
+		Dr("DR_", "Dr.", "Doctor"),
+		Cpt("CAP", "Cpt", "Captain"),
+		Cch("CCH", "Cch", "Coach"),
 
-		Off("OFF", "Off", "Officer"), //
-		Rev("REV", "Rev", "Reverend"), //
-		Fth("FTH", "Fth", "Father"), //
-		PhD("PHD", "PhD", "Professor"), //
-		Mst("MST", "Mst", "Master"); //
+		Off("OFF", "Off", "Officer"),
+		Rev("REV", "Rev", "Reverend"),
+		Fth("FTH", "Fth", "Father"),
+		PhD("PHD", "PhD", "Professor"),
+		Mst("MST", "Mst", "Master");
 
 		private final String code;
 
@@ -89,35 +89,35 @@ public final class ContactsRestServices implements RestfulService {
 	private final Map<Long, Contact> contacts = new HashMap<>();
 
 	public ContactsRestServices() throws ParseException {
-		appendContact(Honorific.Mr, "Martin", "Jean", parseDate("19/05/1980"), //
-				createAddress("1, rue de Rivoli", "", "Paris", "75001", "France"), //
+		appendContact(Honorific.Mr, "Martin", "Jean", parseDate("19/05/1980"),
+				createAddress("1, rue de Rivoli", "", "Paris", "75001", "France"),
 				"jean.martin@gmail.com", "01 02 03 04 05");
-		appendContact(Honorific.Miss, "Dubois", "Marie", parseDate("20/06/1981"), //
-				createAddress("2, rue Beauregard", "", "Paris", "75002", "France"), //
+		appendContact(Honorific.Miss, "Dubois", "Marie", parseDate("20/06/1981"),
+				createAddress("2, rue Beauregard", "", "Paris", "75002", "France"),
 				"marie.dubois@gmail.com", "01 13 14 15 16");
-		appendContact(Honorific.Cpt, "Petit", "Philippe", parseDate("18/04/1979"), //
-				createAddress("3, rue Meslay", "", "Paris", "75003", "France"), //
+		appendContact(Honorific.Cpt, "Petit", "Philippe", parseDate("18/04/1979"),
+				createAddress("3, rue Meslay", "", "Paris", "75003", "France"),
 				"philippe.petit@gmail.com", "01 24 25 26 27");
-		appendContact(Honorific.Off, "Durant", "Nathalie", parseDate("21/07/1982"), //
-				createAddress("4, avenue Victoria", "", "Paris", "75004", "France"), //
+		appendContact(Honorific.Off, "Durant", "Nathalie", parseDate("21/07/1982"),
+				createAddress("4, avenue Victoria", "", "Paris", "75004", "France"),
 				"nathalie.durant@gmail.com", "01 35 36 37 38");
-		appendContact(Honorific.PhD, "Leroy", "Michel", parseDate("17/03/1978"), //
-				createAddress("5, boulevard Saint-Marcel", "", "Paris", "75005", "France"), //
+		appendContact(Honorific.PhD, "Leroy", "Michel", parseDate("17/03/1978"),
+				createAddress("5, boulevard Saint-Marcel", "", "Paris", "75005", "France"),
 				"michel.leroy@gmail.com", "01 46 47 48 49");
-		appendContact(Honorific.Ms, "Moreau", "Isabelle", parseDate("22/08/1983"), //
-				createAddress("6, boulevard Raspail", "", "Paris", "75006", "France"), //
+		appendContact(Honorific.Ms, "Moreau", "Isabelle", parseDate("22/08/1983"),
+				createAddress("6, boulevard Raspail", "", "Paris", "75006", "France"),
 				"isabelle.moreau@gmail.com", "01 57 58 59 50");
-		appendContact(Honorific.Rev, "Lefebvre", "Alain", parseDate("16/02/1977"), //
-				createAddress("7, rue Cler", "", "Paris", "75007", "France"), //
+		appendContact(Honorific.Rev, "Lefebvre", "Alain", parseDate("16/02/1977"),
+				createAddress("7, rue Cler", "", "Paris", "75007", "France"),
 				"alain.lefebvre@gmail.com", "01 68 69 60 61");
-		appendContact(Honorific.Dr, "Garcia", "Sylvie", parseDate("23/09/1984"), //
-				createAddress("8, rue de Ponthieu", "", "Paris", "75008", "France"), //
+		appendContact(Honorific.Dr, "Garcia", "Sylvie", parseDate("23/09/1984"),
+				createAddress("8, rue de Ponthieu", "", "Paris", "75008", "France"),
 				"sylvie.garcia@gmail.com", "01 79 70 71 72");
-		appendContact(Honorific.Mst, "Roux", "Patrick", parseDate("15/01/1976"), //
-				createAddress("9, avenue Frochot", "", "Paris", "75009", "France"), //
+		appendContact(Honorific.Mst, "Roux", "Patrick", parseDate("15/01/1976"),
+				createAddress("9, avenue Frochot", "", "Paris", "75009", "France"),
 				"patrick.roux@gmail.com", "01 80 81 82 83");
-		appendContact(Honorific.Mrs, "Fournier", "Catherine", parseDate("24/10/1985"), //
-				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"), //
+		appendContact(Honorific.Mrs, "Fournier", "Catherine", parseDate("24/10/1985"),
+				createAddress("10, avenue Claude Vellefaux", "", "Paris", "75010", "France"),
 				"catherine.fournier@gmail.com", "01 91 92 93 94");
 	}
 
