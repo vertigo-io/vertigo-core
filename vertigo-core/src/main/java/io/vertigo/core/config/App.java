bbfa0de5f2b2a52a12d69e76c8b73f5e57b0351a
phlppchrtn <pchretien55@gmail.com>
2014-12-21 19:53:10 +0100

    [core]fusion app and Home

--- vertigo-core/src/main/java/io/vertigo/core/config/App.java
+++ vertigo-core/src/main/java/io/vertigo/core/config/App.java
@@ -19,22 +19,21 @@
 package io.vertigo.core.config;
 
 import io.vertigo.core.Home;
-import io.vertigo.lang.Assertion;
 
 /**
  * Application state.
  */
 public final class App implements AutoCloseable {
-	private final AppConfig appConfig;
+	//	private final AppConfig appConfig;
 
 	/**
 	 * Constructor.
 	 * @param appConfig App Config
 	 */
 	public App(final AppConfig appConfig) {
-		Assertion.checkNotNull(appConfig);
+		//		Assertion.checkNotNull(appConfig);
 		//---------------------------------------------------------------------
-		this.appConfig = appConfig;
+		//	this.appConfig = appConfig;
 		Home.start(appConfig);
 	}
 
@@ -43,11 +42,11 @@ public final class App implements AutoCloseable {
 	public void close() {
 		Home.stop();
 	}
-
-	/**
-	 * @return Environment Parameters
-	 */
-	public AppConfig getAppConfig() {
-		return appConfig;
-	}
+	//
+	//	/**
+	//	 * @return Environment Parameters
+	//	 */
+	//	public AppConfig getAppConfig() {
+	//		return appConfig;
+	//	}
 }
