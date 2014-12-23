b694bca4dd19d92d9a67e7ae40e14f10f99e97d3
NPi2Loup <npiedeloup@gmail.com>
2014-12-23 17:27:51 +0100

    [Vega] Changed RateLimiting window from 15m to 5m, should reduce errors when testing

--- vertigo-vega-impl/src/main/java/io/vertigo/vega/impl/rest/handler/RateLimitingHandler.java
+++ vertigo-vega-impl/src/main/java/io/vertigo/vega/impl/rest/handler/RateLimitingHandler.java
@@ -44,7 +44,7 @@ import spark.Response;
  */
 public final class RateLimitingHandler implements Activeable, RouteHandler {
 	private static final long DEFAULT_LIMIT_VALUE = 150; //the rate limit ceiling value
-	private static final long DEFAULT_WINDOW_SECONDS = 15 * 60; //the time windows use to limit calls rate
+	private static final long DEFAULT_WINDOW_SECONDS = 5 * 60; //the time windows use to limit calls rate
 	private static final String RATE_LIMIT_LIMIT = "X-Rate-Limit-Limit"; //the rate limit ceiling for that given request
 	private static final String RATE_LIMIT_REMAINING = "X-Rate-Limit-Remaining"; //the number of requests left for the M minute window
 	private static final String RATE_LIMIT_RESET = "X-Rate-Limit-Reset"; //the remaining seconds before the rate limit resets
