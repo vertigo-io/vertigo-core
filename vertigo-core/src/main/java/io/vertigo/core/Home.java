81f49146b5aa7c43b268a071841e1d8b3b9a6d7c
NPi2Loup <npiedeloup@gmail.com>
2014-12-23 14:11:33 +0100

    [Core] Simpler external logConfig

--- vertigo-core/src/main/java/io/vertigo/core/Home.java
+++ vertigo-core/src/main/java/io/vertigo/core/Home.java
@@ -29,7 +29,6 @@ import java.io.File;
 import java.net.URL;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.PropertyConfigurator;
 import org.apache.log4j.xml.DOMConfigurator;
 
 /**
@@ -126,7 +125,7 @@ public final class Home {
 		Assertion.checkNotNull(appConfig);
 		//-----
 		change(State.INACTIVE, State.starting);
-		this.MyAppConfig = appConfig;
+		MyAppConfig = appConfig;
 		try {
 			Assertion.checkState(definitionSpace.isEmpty(), "DefinitionSpace must be empty");
 			//---
@@ -194,25 +193,17 @@ public final class Home {
 		Assertion.checkNotNull(log4Config);
 		//-----
 		final String log4jFileName = log4Config.getFileName();
-		final boolean log4jFormatXml = log4jFileName.endsWith(".xml");
+		Assertion.checkArgument(log4jFileName.endsWith(".xml"), "Use the XML format for log4j configurations (instead of : {0}).", log4jFileName);
 		final URL url = Home.class.getResource(log4jFileName);
 		if (url != null) {
-			if (log4jFormatXml) {
-				DOMConfigurator.configure(url);
-			} else {
-				PropertyConfigurator.configure(url);
-			}
+			DOMConfigurator.configure(url);
 			Logger.getRootLogger().info("Log4J configuration chargée (resource) : " + url.getFile());
 		} else {
 			Assertion.checkArgument(new File(log4jFileName).exists(), "Fichier de configuration log4j : {0} est introuvable", log4jFileName);
 			// Avec configureAndWatch (utilise un anonymous thread)
 			// on peut modifier à chaud le fichier de conf log4j
 			// mais en cas de hot-deploy, le thread reste présent ce qui peut-entrainer des problèmes.
-			if (log4jFormatXml) {
-				DOMConfigurator.configureAndWatch(log4jFileName);
-			} else {
-				PropertyConfigurator.configureAndWatch(log4jFileName);
-			}
+			DOMConfigurator.configureAndWatch(log4jFileName);
 		}
 		Logger.getRootLogger().info("Log4J configuration chargée (fichier) : " + log4jFileName);
 	}
