6d4d159bf77f0725df92b9725fbc7f036ef4ba41
Philippe Chrétien <pchretien55@gmail.com>
2015-01-12 18:11:27 +0100

    [dynamo] + simple

--- vertigo-dynamo-impl/src/main/java/io/vertigo/dynamo/impl/search/SearchManagerImpl.java
+++ vertigo-dynamo-impl/src/main/java/io/vertigo/dynamo/impl/search/SearchManagerImpl.java
@@ -29,7 +29,6 @@ import io.vertigo.dynamo.search.metamodel.IndexDefinition;
 import io.vertigo.dynamo.search.model.Index;
 import io.vertigo.dynamo.search.model.SearchQuery;
 import io.vertigo.lang.Assertion;
-import io.vertigo.lang.Option;
 
 import java.util.Collection;
 
@@ -40,64 +39,58 @@ import javax.inject.Inject;
  * @author dchallas
  */
 public final class SearchManagerImpl implements SearchManager {
-	private final Option<SearchServicesPlugin> searchServicesPlugin;
+	private final SearchServicesPlugin searchServicesPlugin;
 
 	/**
 	 * Constructor.
 	 * @param searchServicesPlugin Search plugin
 	 */
 	@Inject
-	public SearchManagerImpl(final Option<SearchServicesPlugin> searchServicesPlugin) {
+	public SearchManagerImpl(final SearchServicesPlugin searchServicesPlugin) {
 		Assertion.checkNotNull(searchServicesPlugin);
 		//-----
 		this.searchServicesPlugin = searchServicesPlugin;
 	}
 
-	private SearchServicesPlugin getSearchServices() {
-		Assertion.checkArgument(searchServicesPlugin.isDefined(), "Aucun plugin de recherche déclaré");
-		//-----
-		return searchServicesPlugin.get();
-	}
-
 	/** {@inheritDoc} */
 	@Override
 	public void registerIndexFieldNameResolver(final IndexDefinition indexDefinition, final IndexFieldNameResolver indexFieldNameResolver) {
-		getSearchServices().registerIndexFieldNameResolver(indexDefinition, indexFieldNameResolver);
+		searchServicesPlugin.registerIndexFieldNameResolver(indexDefinition, indexFieldNameResolver);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public <I extends DtObject, R extends DtObject> void putAll(final IndexDefinition indexDefinition, final Collection<Index<I, R>> indexCollection) {
-		getSearchServices().putAll(indexDefinition, indexCollection);
+		searchServicesPlugin.putAll(indexDefinition, indexCollection);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public <I extends DtObject, R extends DtObject> void put(final IndexDefinition indexDefinition, final Index<I, R> index) {
-		getSearchServices().put(indexDefinition, index);
+		searchServicesPlugin.put(indexDefinition, index);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public <R extends DtObject> FacetedQueryResult<R, SearchQuery> loadList(final SearchQuery searchQuery, final FacetedQuery facetedQuery) {
-		return getSearchServices().loadList(searchQuery, facetedQuery);
+		return searchServicesPlugin.loadList(searchQuery, facetedQuery);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public long count(final IndexDefinition indexDefinition) {
-		return getSearchServices().count(indexDefinition);
+		return searchServicesPlugin.count(indexDefinition);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public void remove(final IndexDefinition indexDefinition, final URI uri) {
-		getSearchServices().remove(indexDefinition, uri);
+		searchServicesPlugin.remove(indexDefinition, uri);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public void remove(final IndexDefinition indexDefinition, final ListFilter listFilter) {
-		getSearchServices().remove(indexDefinition, listFilter);
+		searchServicesPlugin.remove(indexDefinition, listFilter);
 	}
 }
