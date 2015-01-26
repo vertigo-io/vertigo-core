32c55cf320d05cec9da25c03a3b11deda8b06017
NPi2Loup <npiedeloup@gmail.com>
2015-01-26 14:45:55 +0100

    [Dynamo] Simpler WhereIn with regexp

--- vertigo-dynamo-impl/src/main/java/io/vertigo/dynamox/task/WhereInPreProcessor.java
+++ vertigo-dynamo-impl/src/main/java/io/vertigo/dynamox/task/WhereInPreProcessor.java
@@ -19,27 +19,34 @@
 package io.vertigo.dynamox.task;
 
 import io.vertigo.dynamo.domain.metamodel.DataType;
-import io.vertigo.dynamo.domain.metamodel.Domain;
 import io.vertigo.dynamo.domain.model.DtList;
 import io.vertigo.dynamo.domain.model.DtObject;
 import io.vertigo.dynamo.task.metamodel.TaskAttribute;
 import io.vertigo.lang.Assertion;
+import io.vertigo.util.StringUtil;
 
-import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
- * Ce processor permet de remplacer le Where XXX_ID in (YYY.ROWNUM.XXX_ID).
+ * Ce processor permet de remplacer le Where XXX_ID in (#YYY.ROWNUM.ZZZ_ID#).
  * @author npiedeloup
  */
 final class WhereInPreProcessor {
 
-	//private static final String REGEXP_PATTERN = "\\s([A-Z0-9_]+)\\s+((?:NOT\\s+)|(?:not\\s+))?(?:in|IN)\\s+\\(#([A-Z0-9_]+)\\.(?:ROWNUM|rownum)\\.([A-Z0-9_]+)#\\)";
-	//private static final Pattern JAVA_PATTERN = Pattern.compile(REGEXP_PATTERN);
-	private static final String ROWNUM = ".ROWNUM.";
+	private static final int MASTER_TABLE_FK_GROUP = 1;
+	private static final int OPTIONNAL_NOT_GROUP = 2;
+	private static final int DTC_INPUTNAME_GROUP = 3;
+	private static final int DTC_INPUT_PK_GROUP = 4;
+
+	private static final String REGEXP_CHECK_PATTERN = "\\s(?:IN|in).+#.+(?:ROWNUM|rownum).+#";
+	private static final Pattern JAVA_CHECK_PATTERN = Pattern.compile(REGEXP_CHECK_PATTERN);
+
+	private static final String REGEXP_PATTERN = "\\s([A-Z0-9_\\.]+)\\s+((?:NOT|not)\\s+)?(?:IN|in)\\s+\\(#([A-Z0-9_]+)\\.(?:ROWNUM|rownum)\\.([A-Z0-9_]+)#\\)";
+	private static final Pattern JAVA_PATTERN = Pattern.compile(REGEXP_PATTERN);
+
 	private static final int NB_MAX_WHERE_IN_ITEM = 1000;
-	private static final String STR_IN = " IN";
-	private static final String STR_NOT_IN = " NOT IN";
 	private static final char IN_CHAR = '#';
 
 	private final Map<TaskAttribute, Object> parameterValuesMap;
@@ -68,121 +75,80 @@ final class WhereInPreProcessor {
 
 	private static boolean containsKeywords(final String sqlQuery) {
 		//On vérifie la précense de tous les mots clés (.rownum., in, #)
-		return containsIgnoreCase(sqlQuery, ROWNUM) && containsIgnoreCase(sqlQuery, STR_IN) && sqlQuery.indexOf(IN_CHAR) > 1;
+		return JAVA_CHECK_PATTERN.matcher(sqlQuery).find(); //fast check
+	}
+
+	private TaskAttribute obtainTaskAttribute(final String attributeName) {
+		for (final TaskAttribute attribute : parameterValuesMap.keySet()) {
+			if (attribute.getName().equals(attributeName)) {
+				return attribute;
+			}
+		}
+		throw new IllegalStateException(StringUtil.format("Attribute {0} not found.", attributeName));
 	}
 
 	private String doEvaluate(final String sqlQuery) {
-		//TODO : prévoir une implémentation plus simple à base de regexp et de contruction du séquentiel du résultat plutot que par replaceAll
-		String query = sqlQuery;
-		final StringBuilder subQuery = new StringBuilder();
-		final StringBuilder nameSearchTemp = new StringBuilder();
-		for (final TaskAttribute attribute : parameterValuesMap.keySet()) {
-			final Domain domain = attribute.getDomain();
-			if (attribute.isIn() && domain.getDataType() == DataType.DtList) {
-				final String outParamName = attribute.getName();
-				nameSearchTemp.setLength(0);
-				nameSearchTemp.append(IN_CHAR);
-				nameSearchTemp.append(outParamName);
-				final int indexStart = query.indexOf(nameSearchTemp.toString());
-				if (indexStart != -1) {
-					final int indexEnd = query.indexOf(IN_CHAR, indexStart + 1);
-					Assertion.checkState(indexEnd > 0, "La fin du paramètre est introuvable ({0})", nameSearchTemp.toString());
-					final String strDtc = query.substring(indexStart, indexEnd + 1);
-					Assertion.checkState(!strDtc.contains("rownum") && strDtc.contains("ROWNUM"), "Le mot clé ROWNUM est attendu en majuscule ({0})", strDtc);
-					//-----
-					//TG : recherche parentheses + IN & NOT IN pour contraintre oracle (pas plus de 1000 elements dans clause IN)
-					final int indexFirstParenthesis = query.lastIndexOf("(", indexStart + 1);
-					int indexOfIn = lastIndexOfIgnoreCase(query, STR_IN, indexFirstParenthesis + 1);
-					final int indexOfNotIn = lastIndexOfIgnoreCase(query, STR_NOT_IN, indexFirstParenthesis + 1);
-					//Check si NOT IN ou IN
-					boolean isNotIn = false;
-					if (indexOfNotIn != -1 && indexOfIn - indexOfNotIn < 5) {
-						indexOfIn = indexOfNotIn;
-						isNotIn = true;
-					}
-					final int indexOfSecondSpace = lastIndexOfWhiteSpace(query, indexOfIn + 1);
-					final int indexOfFirstSpace = lastIndexOfWhiteSpace(query, indexOfSecondSpace - 1);
-					final String fkColumnName = query.substring(indexOfFirstSpace + 1, indexOfSecondSpace).trim();
-					final int indexRownum = strDtc.indexOf(ROWNUM);
-					final String fieldName = strDtc.substring(indexRownum + ROWNUM.length(), strDtc.length() - 1);
+		final StringBuilder buildQuery = new StringBuilder(sqlQuery.length());
+		int lastMatchOffset = 0;
+		final Matcher matcher = JAVA_PATTERN.matcher(sqlQuery);
+		while (matcher.find()) {
+			//The first matched char is a blank, we keep it (matcher.start+1)
+			buildQuery.append(sqlQuery.substring(lastMatchOffset, matcher.start() + 1));
+			lastMatchOffset = matcher.end();
+			final String fkFieldName = matcher.group(MASTER_TABLE_FK_GROUP);
+			final String pkFieldName = matcher.group(DTC_INPUT_PK_GROUP);
+			final String inputParamName = matcher.group(DTC_INPUTNAME_GROUP);
+			final boolean isNotIn = matcher.group(OPTIONNAL_NOT_GROUP) != null; //null if not found
+			final TaskAttribute attribute = obtainTaskAttribute(inputParamName);
+			Assertion.checkState(attribute.isIn() && attribute.getDomain().getDataType() == DataType.DtList, "Attribute {0} can't be use in WherInPreProcessor. Check it was declared as IN and is DtList type.", inputParamName);
 
+			//-----
+			final DtList<?> listObject = (DtList<?>) parameterValuesMap.get(attribute);
+			if (listObject.isEmpty()) {
+				//where XX not in <<empty>> => always true
+				//where XX in <<empty>> => always false
+				buildQuery.append(isNotIn ? "1=1" : "1=2");
+			} else {
+				//-----
+				final boolean moreThanOneWhereIn = listObject.size() > NB_MAX_WHERE_IN_ITEM;
+				if (moreThanOneWhereIn) {
+					buildQuery.append("( ");
+				}
+				buildQuery.append(fkFieldName);
+				buildQuery.append(isNotIn ? " NOT IN (" : " IN (");
+				//-----
+				String separator = "";
+				int index = 1;
+				for (final DtObject dto : listObject) {
+					buildQuery
+							.append(separator)
+							.append(IN_CHAR)
+							.append(inputParamName)
+							.append(".")
+							.append(String.valueOf(listObject.indexOf(dto)))
+							.append(".")
+							.append(pkFieldName)
+							.append(IN_CHAR);
+					separator = ",";
 					//-----
-					final DtList<?> listObject = (DtList<?>) parameterValuesMap.get(attribute);
-					if (listObject.isEmpty()) {
-						//Liste vide
-						//-----
-						//TG : recherche parentheses + IN & NOT IN pour contraintre oracle (pas plus de 1000 elements dans clause IN)
-						query = query.replace(query.substring(indexOfFirstSpace + 1, query.indexOf(")", indexEnd) + 1), isNotIn ? "1=1" : "1=2");
-						final DataType dataType = listObject.getDefinition().getField(fieldName).getDomain().getDataType();
-						if (dataType == DataType.Integer || dataType == DataType.Long) {
-							query = query.replace(strDtc, "-13371337");
-						} else {
-							query = query.replace(strDtc, "'SHOULD_NOT_MATCH'");
-						}
-					} else {
-						//-----
-						//TG : recherche parentheses + IN & NOT IN pour contraintre oracle (pas plus de 1000 elements dans clause IN)
-						if (listObject.size() > NB_MAX_WHERE_IN_ITEM) {
-							//supprime la parenthese apres le IN
-							String query1 = query.substring(0, indexFirstParenthesis);
-							String query2 = query.substring(indexFirstParenthesis + 1, query.length());
-							query = query1 + query2;
-							//rajoute une parenthese avant la pram de recherche
-							query1 = query.substring(0, indexOfFirstSpace + 1);
-							query2 = query.substring(indexOfFirstSpace + 1, query.length());
-							query = query1 + "(" + query2;
-						}
-						//-----
-						String separator = "";
-						int index = 1;
-						for (final DtObject dto : listObject) {
-							if (listObject.size() > NB_MAX_WHERE_IN_ITEM && index == 1) {
-								subQuery.append("(");
-							}
-							subQuery
-									.append(separator)
-									.append(IN_CHAR)
-									.append(outParamName)
-									.append(".")
-									.append(String.valueOf(listObject.indexOf(dto)))
-									.append(".")
-									.append(fieldName)
-									.append(IN_CHAR);
-
-							separator = ",";
-							//-----
-							//TG : recherche parentheses + IN & NOT IN pour contraintre oracle (pas plus de 1000 elements dans clause IN)
-							if (listObject.size() > NB_MAX_WHERE_IN_ITEM) {
-								if (index == listObject.size()) {
-									subQuery.append(")");
-								} else if (index % NB_MAX_WHERE_IN_ITEM == 0) {
-									subQuery.append(isNotIn ? ") AND " : ") OR ");
-									subQuery.append(fkColumnName);
-									subQuery.append(isNotIn ? " NOT IN (" : " IN (");
-									separator = "";
-								}
-							}
-							//-----
-							index++;
-						}
-						query = query.replace(strDtc, subQuery.toString());
-						subQuery.setLength(0);
+					if (moreThanOneWhereIn && index != listObject.size() && index % NB_MAX_WHERE_IN_ITEM == 0) {
+						buildQuery.append(isNotIn ? ") AND " : ") OR ");
+						buildQuery.append(fkFieldName);
+						buildQuery.append(isNotIn ? " NOT IN (" : " IN (");
+						separator = "";
 					}
+					//-----
+					index++;
+				}
+				buildQuery.append(")");
+				if (moreThanOneWhereIn) {
+					buildQuery.append(")");
 				}
 			}
 		}
-		return query;
-	}
+		Assertion.checkState(lastMatchOffset > 0, "WhereInPreProcessor not applied. Keywords found but query doesn't match. Check syntaxe : XXX_ID <<not>> in (#YYY.ROWNUM.ZZZ_ID#) of {0}", sqlQuery);
 
-	private static int lastIndexOfIgnoreCase(final String string, final String searchStringUpperCase, final int fromIndex) {
-		return Math.max(string.lastIndexOf(searchStringUpperCase, fromIndex), string.lastIndexOf(searchStringUpperCase.toLowerCase(Locale.FRENCH), fromIndex));
-	}
-
-	private static boolean containsIgnoreCase(final String sqlQuery, final String searchStringUpperCase) {
-		return sqlQuery.contains(searchStringUpperCase) || sqlQuery.contains(searchStringUpperCase.toLowerCase());
-	}
-
-	private static int lastIndexOfWhiteSpace(final String string, final int fromIndex) {
-		return Math.max(string.lastIndexOf(' ', fromIndex), string.lastIndexOf('\t', fromIndex));
+		buildQuery.append(sqlQuery.substring(lastMatchOffset));
+		return buildQuery.toString();
 	}
 }
