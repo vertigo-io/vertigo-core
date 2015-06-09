ce68b8e0523a556a7c1d8d0ca322f8bf753c19f3
NPi2Loup <npiedeloup@gmail.com>
2015-06-09 16:24:35 +0200

    [Dynamo] Fixed ReindexTasks

--- vertigo-dynamo-impl/src/main/java/io/vertigo/dynamo/impl/search/ReindexAllTask.java
+++ vertigo-dynamo-impl/src/main/java/io/vertigo/dynamo/impl/search/ReindexAllTask.java
@@ -1,16 +1,20 @@
 package io.vertigo.dynamo.impl.search;
 
 import io.vertigo.core.Home;
+import io.vertigo.dynamo.collections.ListFilter;
 import io.vertigo.dynamo.domain.model.DtObject;
 import io.vertigo.dynamo.domain.model.DtSubject;
+import io.vertigo.dynamo.domain.model.URI;
 import io.vertigo.dynamo.search.SearchManager;
 import io.vertigo.dynamo.search.metamodel.SearchChunk;
 import io.vertigo.dynamo.search.metamodel.SearchIndexDefinition;
 import io.vertigo.dynamo.search.metamodel.SearchLoader;
 import io.vertigo.dynamo.search.model.SearchIndex;
+import io.vertigo.lang.Assertion;
 import io.vertigo.util.ClassUtil;
 
 import java.util.Collection;
+import java.util.List;
 
 final class ReindexAllTask<S extends DtSubject> implements Runnable {
 
@@ -26,9 +30,28 @@ final class ReindexAllTask<S extends DtSubject> implements Runnable {
 	public void run() {
 		final Class<S> subjectClass = (Class<S>) ClassUtil.classForName(searchIndexDefinition.getSubjectDtDefinition().getClassCanonicalName(), DtSubject.class);
 		final SearchLoader<S, DtObject> searchLoader = Home.getComponentSpace().resolve(searchIndexDefinition.getSearchLoaderId(), SearchLoader.class);
+		String lastUri = "*";
 		for (final SearchChunk<S> searchChunk : searchLoader.chunk(subjectClass)) {
-			final Collection<SearchIndex<S, DtObject>> searchIndexes = searchLoader.loadData(searchChunk.getAllURIs());
+			final List<URI<S>> uris = searchChunk.getAllURIs();
+			Assertion.checkArgument(!uris.isEmpty(), "The uris list of a SearchChunk can't be empty");
+			//-----
+			final Collection<SearchIndex<S, DtObject>> searchIndexes = searchLoader.loadData(uris);
+			final URI<S> chunkMaxUri = uris.get(uris.size() - 1);
+			final String maxUri = String.valueOf(chunkMaxUri.getId());
+			searchManager.removeAll(searchIndexDefinition, urisRangeToListFilter(lastUri, maxUri));
 			searchManager.putAll(searchIndexDefinition, searchIndexes);
+			lastUri = maxUri;
 		}
+		//On ne retire pas la fin, il y a un risque de retirer les données ajoutées depuis le démarrage de l'indexation
+		//TODO : à valider
+	}
+
+	private ListFilter urisRangeToListFilter(final String firstUri, final String lastUri) {
+		final String indexIdFieldName = searchIndexDefinition.getIndexDtDefinition().getIdField().get().getName();
+		final StringBuilder sb = new StringBuilder();
+		sb.append(indexIdFieldName).append(":[");
+		sb.append(firstUri).append(" TO ").append(lastUri);
+		sb.append("]");
+		return new ListFilter(sb.toString());
 	}
 }
--- vertigo-dynamo-impl/src/main/java/io/vertigo/dynamo/impl/search/ReindexTask.java
+++ vertigo-dynamo-impl/src/main/java/io/vertigo/dynamo/impl/search/ReindexTask.java
@@ -1,6 +1,7 @@
 package io.vertigo.dynamo.impl.search;
 
 import io.vertigo.core.Home;
+import io.vertigo.dynamo.collections.ListFilter;
 import io.vertigo.dynamo.domain.model.DtObject;
 import io.vertigo.dynamo.domain.model.DtSubject;
 import io.vertigo.dynamo.domain.model.URI;
@@ -11,7 +12,9 @@ import io.vertigo.dynamo.search.model.SearchIndex;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 final class ReindexTask implements Runnable {
 
@@ -32,8 +35,37 @@ final class ReindexTask implements Runnable {
 			reindexUris = new ArrayList<>(dirtyElements);
 			dirtyElements.clear();
 		}
-		final SearchLoader searchLoader = Home.getComponentSpace().resolve(searchIndexDefinition.getSearchLoaderId(), SearchLoader.class);
-		final Collection<SearchIndex<DtSubject, DtObject>> searchIndexes = searchLoader.loadData(reindexUris);
-		searchManager.putAll(searchIndexDefinition, searchIndexes);
+		if (!reindexUris.isEmpty()) {
+			final SearchLoader searchLoader = Home.getComponentSpace().resolve(searchIndexDefinition.getSearchLoaderId(), SearchLoader.class);
+			final Collection<SearchIndex<DtSubject, DtObject>> searchIndexes = searchLoader.loadData(reindexUris);
+			removedNotFoundSubject(searchIndexes, reindexUris);
+			searchManager.putAll(searchIndexDefinition, searchIndexes);
+		}
+	}
+
+	private void removedNotFoundSubject(final Collection<SearchIndex<DtSubject, DtObject>> searchIndexes, final List<URI<? extends DtSubject>> reindexUris) {
+		if (searchIndexes.size() < reindexUris.size()) {
+			final Set<URI<? extends DtSubject>> notFoundUris = new LinkedHashSet<>(reindexUris);
+			for (final SearchIndex<DtSubject, DtObject> searchIndex : searchIndexes) {
+				if (notFoundUris.contains(searchIndex.getURI())) {
+					notFoundUris.remove(searchIndex.getURI());
+				}
+			}
+			searchManager.removeAll(searchIndexDefinition, urisToListFilter(notFoundUris));
+		}
+	}
+
+	private ListFilter urisToListFilter(final Set<URI<? extends DtSubject>> removedUris) {
+		final String indexIdFieldName = searchIndexDefinition.getIndexDtDefinition().getIdField().get().getName();
+		final StringBuilder sb = new StringBuilder();
+		sb.append(indexIdFieldName).append(":(");
+		String sep = "";
+		for (final URI<?> uri : removedUris) {
+			sb.append(sep);
+			sb.append(String.valueOf(uri.getId()));
+			sep = " OR ";
+		}
+		sb.append(")");
+		return new ListFilter(sb.toString());
 	}
 }
