5556ba89f63e73caacc4d7aa7ac6ea7b598de208
phlppchrtn <pchretien55@gmail.com>
2015-07-08 11:57:29 +0200

    [core] added Boot

--- vertigo-core/src/main/java/io/vertigo/core/App.java
+++ vertigo-core/src/main/java/io/vertigo/core/App.java
@@ -1,25 +1,17 @@
 package io.vertigo.core;
 
 import io.vertigo.core.config.AppConfig;
-import io.vertigo.core.config.LogConfig;
+import io.vertigo.core.spaces.Boot;
 import io.vertigo.core.spaces.component.ComponentLoader;
 import io.vertigo.core.spaces.component.ComponentSpace;
 import io.vertigo.core.spaces.config.ConfigSpace;
 import io.vertigo.core.spaces.definiton.DefinitionSpace;
 import io.vertigo.dynamo.environment.EnvironmentManager;
 import io.vertigo.dynamo.impl.environment.DefinitionLoader;
-import io.vertigo.lang.Activeable;
 import io.vertigo.lang.Assertion;
-import io.vertigo.lang.Engine;
 import io.vertigo.util.StringUtil;
 
-import java.io.File;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
 
 public final class App implements AutoCloseable {
 	private static enum State {
@@ -40,6 +32,7 @@ public final class App implements AutoCloseable {
 	private final AppConfig appConfig;
 	private State state;
 
+	private final Boot boot;
 	private final DefinitionSpace definitionSpace;
 	private final ComponentSpace componentSpace;
 	private final ConfigSpace configSpace;
@@ -57,9 +50,8 @@ public final class App implements AutoCloseable {
 		state = State.starting;
 		//-----
 		try {
-			if (appConfig.getBootConfig().getLogConfig().isDefined()) {
-				initLog(appConfig.getBootConfig().getLogConfig().get());
-			}
+			boot = new Boot(appConfig.getBootConfig());
+
 			//-----
 			configSpace = new ConfigSpace();
 			definitionSpace = new DefinitionSpace();
@@ -75,14 +67,16 @@ public final class App implements AutoCloseable {
 			if (componentSpace.contains(EnvironmentManagerId)) {
 				final EnvironmentManager environmentManager = componentSpace.resolve(EnvironmentManager.class);
 				final DefinitionLoader definitionLoader = environmentManager.createDefinitionLoader();
+				//-----
 				definitionLoader.injectDefinitions(appConfig.getModuleConfigs());
 			}
 			//-----2. Load all components (and aspects).
 			componentLoader.injectComponents(appConfig.getModuleConfigs());
 			//-----
-			startEngines();
+			boot.start();
 			componentSpace.start();
 			definitionSpace.start();
+			//-----
 			state = State.active;
 		} catch (final Exception e) {
 			close();
@@ -98,9 +92,11 @@ public final class App implements AutoCloseable {
 		state = State.stopping;
 		//-----
 		try {
+			//------
 			definitionSpace.stop();
 			componentSpace.stop();
-			stopEngines();
+			boot.stop();
+			//------
 		} catch (final Exception e) {
 			LOGGER.error("an error occured when stopping", e);
 			//Quel que soit l'état, on part en échec de l'arrét.
@@ -108,25 +104,6 @@ public final class App implements AutoCloseable {
 		} finally {
 			state = State.closed;
 			Home.setApp(null);
-		}
-	}
-
-	private void startEngines() {
-		for (final Engine engine : appConfig.getBootConfig().getEngines()) {
-			if (engine instanceof Activeable) {
-				Activeable.class.cast(engine).start();
-			}
-		}
-	}
-
-	private void stopEngines() {
-		final List<Engine> reverseEngines = new ArrayList<>(appConfig.getBootConfig().getEngines());
-		java.util.Collections.reverse(reverseEngines);
-
-		for (final Engine engine : reverseEngines) {
-			if (engine instanceof Activeable) {
-				Activeable.class.cast(engine).stop();
-			}
 		}
 	}
 
@@ -154,24 +131,5 @@ public final class App implements AutoCloseable {
 
 	ConfigSpace getConfigSpace() {
 		return configSpace;
-	}
-
-	private static void initLog(final LogConfig log4Config) {
-		Assertion.checkNotNull(log4Config);
-		//-----
-		final String log4jFileName = log4Config.getFileName();
-		Assertion.checkArgument(log4jFileName.endsWith(".xml"), "Use the XML format for log4j configurations (instead of : {0}).", log4jFileName);
-		final URL url = Home.class.getResource(log4jFileName);
-		if (url != null) {
-			DOMConfigurator.configure(url);
-			Logger.getRootLogger().info("Log4J configuration chargée (resource) : " + url.getFile());
-		} else {
-			Assertion.checkArgument(new File(log4jFileName).exists(), "Fichier de configuration log4j : {0} est introuvable", log4jFileName);
-			// Avec configureAndWatch (utilise un anonymous thread)
-			// on peut modifier à chaud le fichier de conf log4j
-			// mais en cas de hot-deploy, le thread reste présent ce qui peut-entrainer des problèmes.
-			DOMConfigurator.configureAndWatch(log4jFileName);
-		}
-		Logger.getRootLogger().info("Log4J configuration chargée (fichier) : " + log4jFileName);
 	}
 }
--- vertigo-core/src/main/java/io/vertigo/core/spaces/Boot.java
+++ vertigo-core/src/main/java/io/vertigo/core/spaces/Boot.java
@@ -1,5 +1,78 @@
 package io.vertigo.core.spaces;
 
-public final class Boot {
+import io.vertigo.core.Home;
+import io.vertigo.core.config.BootConfig;
+import io.vertigo.core.config.LogConfig;
+import io.vertigo.lang.Activeable;
+import io.vertigo.lang.Assertion;
+import io.vertigo.lang.Engine;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+public final class Boot implements Activeable {
+	private final BootConfig bootConfig;
+
+	public Boot(final BootConfig bootConfig) {
+		Assertion.checkNotNull(bootConfig);
+		//-----
+		this.bootConfig = bootConfig;
+		//-----
+		if (bootConfig.getLogConfig().isDefined()) {
+			initLog(bootConfig.getLogConfig().get());
+		}
+	}
+
+	@Override
+	public void start() {
+		startEngines();
+	}
+
+	private void startEngines() {
+		for (final Engine engine : bootConfig.getEngines()) {
+			if (engine instanceof Activeable) {
+				Activeable.class.cast(engine).start();
+			}
+		}
+	}
+
+	@Override
+	public void stop() {
+		stopEngines();
+	}
+
+	private void stopEngines() {
+		final List<Engine> reverseEngines = new ArrayList<>(bootConfig.getEngines());
+		java.util.Collections.reverse(reverseEngines);
+
+		for (final Engine engine : reverseEngines) {
+			if (engine instanceof Activeable) {
+				Activeable.class.cast(engine).stop();
+			}
+		}
+	}
+
+	private static void initLog(final LogConfig log4Config) {
+		Assertion.checkNotNull(log4Config);
+		//-----
+		final String log4jFileName = log4Config.getFileName();
+		Assertion.checkArgument(log4jFileName.endsWith(".xml"), "Use the XML format for log4j configurations (instead of : {0}).", log4jFileName);
+		final URL url = Home.class.getResource(log4jFileName);
+		if (url != null) {
+			DOMConfigurator.configure(url);
+			Logger.getRootLogger().info("Log4J configuration chargée (resource) : " + url.getFile());
+		} else {
+			Assertion.checkArgument(new File(log4jFileName).exists(), "Fichier de configuration log4j : {0} est introuvable", log4jFileName);
+			// Avec configureAndWatch (utilise un anonymous thread)
+			// on peut modifier à chaud le fichier de conf log4j
+			// mais en cas de hot-deploy, le thread reste présent ce qui peut-entrainer des problèmes.
+			DOMConfigurator.configureAndWatch(log4jFileName);
+		}
+		Logger.getRootLogger().info("Log4J configuration chargée (fichier) : " + log4jFileName);
+	}
 }
