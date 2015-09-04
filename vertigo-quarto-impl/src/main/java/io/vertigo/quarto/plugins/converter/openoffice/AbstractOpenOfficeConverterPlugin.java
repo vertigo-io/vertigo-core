5ef7295913959902a836adc684f8ff407a521b4a
NPi2Loup <npiedeloup@gmail.com>
2015-09-04 14:07:04 +0200

    [quarto] Error msg clearer

--- vertigo-quarto-impl/src/main/java/io/vertigo/quarto/plugins/converter/openoffice/AbstractOpenOfficeConverterPlugin.java
+++ vertigo-quarto-impl/src/main/java/io/vertigo/quarto/plugins/converter/openoffice/AbstractOpenOfficeConverterPlugin.java
@@ -119,7 +119,7 @@ abstract class AbstractOpenOfficeConverterPlugin implements ConverterPlugin, Act
 			final Future<File> targetFileFuture = executors.submit(convertTask);
 			targetFile = targetFileFuture.get(convertTimeoutSeconds, TimeUnit.SECONDS);
 		} catch (final Exception e) {
-			throw new RuntimeException("Erreur de conversion du document au format " + targetFormat.name(), e);
+			throw new RuntimeException("Erreur de conversion du document au format " + targetFormat.name() + " (" + e.getClass().getSimpleName() + ")", e);
 		}
 		return fileManager.createFile(targetFile);
 	}
