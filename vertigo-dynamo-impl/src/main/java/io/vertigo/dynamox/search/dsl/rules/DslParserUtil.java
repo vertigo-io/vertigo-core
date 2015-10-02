b7c00869468a0f93b50bb8ba27cf98907d1c7dce
NPi2Loup <npiedeloup@gmail.com>
2015-10-02 10:40:19 +0200

    [Dynamo] Javadoc

--- vertigo-dynamo-impl/src/main/java/io/vertigo/dynamox/search/dsl/rules/DslParserUtil.java
+++ vertigo-dynamo-impl/src/main/java/io/vertigo/dynamox/search/dsl/rules/DslParserUtil.java
@@ -9,8 +9,21 @@ import io.vertigo.dynamox.search.dsl.definition.DslUserCriteria;
 
 import java.util.List;
 
+/**
+ * Util for parsing search patterns and queries.
+ * @author npiedeloup
+ */
 public final class DslParserUtil {
 
+	private DslParserUtil() {
+		//nothing
+	}
+
+	/**
+	 * @param buildQuery Builder pattern
+	 * @return Parsed pattern
+	 * @throws NotFoundException If pattern doesn't match grammar
+	 */
 	public static List<DslMultiExpressionDefinition> parseMultiExpression(final String buildQuery) throws NotFoundException {
 		final Rule<DslMultiExpressionDefinition> expressionsRule = new DslMultiExpressionRule();
 		final ManyRule<DslMultiExpressionDefinition> many = new ManyRule<>(expressionsRule, false, true); //repeat true => on veut tout la chaine
@@ -19,7 +32,11 @@ public final class DslParserUtil {
 		return parser.get();
 	}
 
-	public static List<DslUserCriteria> parseUserCriteria(String userString) {
+	/**
+	 * @param userString User criteria
+	 * @return Parsed User criteria
+	 */
+	public static List<DslUserCriteria> parseUserCriteria(final String userString) {
 		return DslUserCriteriaRule.parse(userString);
 	}
 }
