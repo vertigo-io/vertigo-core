6fbd33e44dc2f71a24c9fe04047c21ba870a183f
NPi2Loup <npiedeloup@gmail.com>
2016-02-22 18:02:42 +0100

    [Vega] Updated rest-assured version

--- vertigo-vega-impl/src/test/java/io/vertigo/vega/webservice/WebServiceManagerTest.java
+++ vertigo-vega-impl/src/test/java/io/vertigo/vega/webservice/WebServiceManagerTest.java
@@ -1553,7 +1553,7 @@ public final class WebServiceManagerTest {
 				.formParam("id", 12)
 				.formParam("note", "Some very important notes about this file.")
 				.expect()
-				.body("globalErrors", Matchers.contains("File upfile not found. Parts sent : id, upFile, note"))
+				.body("globalErrors", Matchers.contains("File upfile not found. Parts sent : note, upFile, id"))
 				.statusCode(HttpStatus.SC_INTERNAL_SERVER_ERROR)
 				.when()
 				.post("/test/uploadFile");
