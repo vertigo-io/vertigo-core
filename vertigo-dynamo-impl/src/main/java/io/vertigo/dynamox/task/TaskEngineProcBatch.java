7d83d0fbdd529c18683593b83b5faabe45dd7547
phlppchrtn <pchretien55@gmail.com>
2016-06-22 00:58:39 +0200

    [dynamo]style

--- vertigo-dynamo-impl/src/main/java/io/vertigo/dynamox/task/TaskEngineProcBatch.java
+++ vertigo-dynamo-impl/src/main/java/io/vertigo/dynamox/task/TaskEngineProcBatch.java
@@ -21,6 +21,10 @@
  */
 package io.vertigo.dynamox.task;
 
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+
 import io.vertigo.commons.script.ScriptManager;
 import io.vertigo.dynamo.database.SqlDataBaseManager;
 import io.vertigo.dynamo.database.connection.SqlConnection;
@@ -33,14 +37,10 @@ import io.vertigo.dynamo.task.metamodel.TaskAttribute;
 import io.vertigo.dynamo.transaction.VTransactionManager;
 import io.vertigo.lang.Assertion;
 
-import java.sql.SQLException;
-
-import javax.inject.Inject;
-
 /**
  * @author jmforhan
  */
-public class TaskEngineProcBatch extends AbstractTaskEngineSQL<SqlCallableStatement> {
+public final class TaskEngineProcBatch extends AbstractTaskEngineSQL<SqlCallableStatement> {
 	/**
 	 * Constructeur.
 	 * @param scriptManager Manager de traitment de scripts
@@ -97,9 +97,7 @@ public class TaskEngineProcBatch extends AbstractTaskEngineSQL<SqlCallableStatem
 				batchSize = dtc.size();
 			}
 		}
-		if (batchSize == null) {
-			throw new IllegalArgumentException("Pour un traitement Batch, il doit y avoir une (et une seule) liste en entrée.");
-		}
+		Assertion.checkNotNull(batchSize, "Pour un traitement Batch, il doit y avoir une (et une seule) liste en entrée.");
 		return batchSize;
 	}
 }
