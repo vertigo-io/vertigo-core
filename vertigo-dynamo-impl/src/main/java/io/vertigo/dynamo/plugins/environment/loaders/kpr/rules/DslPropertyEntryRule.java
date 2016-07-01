a979965f9bc55250ac73b78962df25adb077955d
NPi2Loup <npiedeloup@gmail.com>
2016-07-01 17:39:42 +0200

    [All] Updated license

--- vertigo-dynamo-impl/src/main/java/io/vertigo/dynamo/plugins/environment/loaders/kpr/rules/DslPropertyEntryRule.java
+++ vertigo-dynamo-impl/src/main/java/io/vertigo/dynamo/plugins/environment/loaders/kpr/rules/DslPropertyEntryRule.java
@@ -1,7 +1,7 @@
 /**
  * vertigo - simple java starter
  *
- * Copyright (C) 2013, KleeGroup, direction.technique@kleegroup.com (http://www.kleegroup.com)
+ * Copyright (C) 2013-2016, KleeGroup, direction.technique@kleegroup.com (http://www.kleegroup.com)
  * KleeGroup, Centre d'affaire la Boursidiere - BP 159 - 92357 Le Plessis Robinson Cedex - France
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
