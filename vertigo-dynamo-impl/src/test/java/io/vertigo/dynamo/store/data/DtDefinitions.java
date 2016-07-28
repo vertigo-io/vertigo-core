9afbb079841569b7c147c0ce646c4f52e40590e5
phlppchrtn <pchretien55@gmail.com>
2016-07-28 12:45:07 +0200

    [dynamo] + simple

--- vertigo-dynamo-impl/src/test/java/io/vertigo/dynamo/store/data/DtDefinitions.java
+++ vertigo-dynamo-impl/src/test/java/io/vertigo/dynamo/store/data/DtDefinitions.java
@@ -18,98 +18,23 @@
  */
 package io.vertigo.dynamo.store.data;
 
-import java.util.Arrays;
 import java.util.Iterator;
 
-import io.vertigo.dynamo.domain.metamodel.DtFieldName;
+import io.vertigo.dynamo.store.data.domain.car.Car;
+import io.vertigo.dynamo.store.data.domain.famille.Famille;
+import io.vertigo.dynamo.store.data.domain.fileinfo.VxFileInfo;
+import io.vertigo.util.ListBuilder;
 
-/**
- * Attention cette classe est générée automatiquement !
- */
 public final class DtDefinitions implements Iterable<Class<?>> {
-	
-	/**
-	 * Enumération des DtDefinitions.
-	 */
-	public enum Definitions {
-			/** Objet de données Car. */
-			Car(io.vertigo.dynamo.store.data.domain.car.Car.class),
-			/** Objet de données Famille. */
-			Famille(io.vertigo.dynamo.store.data.domain.famille.Famille.class),
-		;
-		
-		private final Class<?> clazz;
-		private Definitions(final Class<?> clazz) {
-			this.clazz = clazz;
-		}
-		
-		/** 
-		  * Classe associée.
-		  * @return Class d'implémentation de l'objet 
-		  */
-		public Class<?> getDtClass() {
-			return clazz;
-		}
-    }
 
-	/**
-	 * Enumération des champs de Car.
-	 */
-	public enum CarFields implements DtFieldName {
-		/** Propriété 'identifiant de la voiture'. */
-		ID,
-		/** Propriété 'Constructeur'. */
-		MAKE,
-		/** Propriété 'ModÃ¨le'. */
-		MODEL,
-		/** Propriété 'Descriptif'. */
-		DESCRIPTION, 
-		/** Propriété 'AnnÃ©e'. */
-		YEAR,
-		/** Propriété 'KilomÃ©trage'. */
-		KILO,
-		/** Propriété 'Prix'. */
-		PRICE, 
-		/** Propriété 'Type de moteur'. */
-		MOTOR_TYPE,
+	@Override
+	public Iterator<Class<?>> iterator() {
+		return new ListBuilder<Class<?>>()
+				.add(Car.class)
+				.add(Famille.class)
+				.add(VxFileInfo.class)
+				.build()
+				.iterator();
 	}
 
-	/**
-	 * Enumération des champs de Famille.
-	 */
-	public enum FamilleFields implements DtFieldName {
-		/** Propriété 'identifiant de la famille'. */
-		FAM_ID,
-		/** Propriété 'Libelle'. */
-		LIBELLE,
-		/** Propriété 'Libelle'. */
-		DESCRIPTION,
-	}
-
-	    
-    /** {@inheritDoc} */
-    @Override
-    public Iterator<Class<?>> iterator() {
-        return new Iterator<Class<?>>() {
-            private Iterator<Definitions> it = Arrays.asList(Definitions.values()).iterator();
-
-            /** {@inheritDoc} */
-            @Override
-            public boolean hasNext() {
-				return it.hasNext();
-            }
-
-            /** {@inheritDoc} */
-            @Override
-            public Class<?> next() {
-            	return it.next().getDtClass();
-            }
-
-            /** {@inheritDoc} */
-            @Override
-            public void remove() {
-            	//unsupported
-            }
-        };
-    }                      
 }
