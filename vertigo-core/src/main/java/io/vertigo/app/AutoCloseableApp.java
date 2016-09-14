f04b019629078ace1aa0b9bdeabe5cdbe766ae3e
NPi2Loup <npiedeloup@gmail.com>
2016-09-14 18:07:21 +0200

    [Core] Code style

--- vertigo-core/src/main/java/io/vertigo/app/AutoCloseableApp.java
+++ vertigo-core/src/main/java/io/vertigo/app/AutoCloseableApp.java
@@ -180,7 +180,8 @@ public final class AutoCloseableApp implements App, AutoCloseable {
 
 	private void initializeAllComponents() {
 		for (final ComponentInitializerConfig componentInitializerConfig : appConfig.getComponentInitializerConfigs()) {
-			Assertion.checkArgument(!Activeable.class.isAssignableFrom(componentInitializerConfig.getInitializerClass()), "The initializer '{0}' can't be activeable", componentInitializerConfig.getInitializerClass());
+			Assertion.checkArgument(!Activeable.class.isAssignableFrom(componentInitializerConfig.getInitializerClass()),
+					"The initializer '{0}' can't be activeable", componentInitializerConfig.getInitializerClass());
 			final ComponentInitializer componentInitializer = Injector.newInstance(componentInitializerConfig.getInitializerClass(), componentSpace);
 			componentInitializer.init();
 		}
