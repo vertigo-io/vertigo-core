8313decf0993986c19d4bdb61127f69f1769e98e
NPi2Loup <npiedeloup@gmail.com>
2016-10-05 17:59:18 +0200

    [Core] AspectAnnotation target Annotations only

--- vertigo-core/src/main/java/io/vertigo/core/component/aop/AspectAnnotation.java
+++ vertigo-core/src/main/java/io/vertigo/core/component/aop/AspectAnnotation.java
@@ -5,7 +5,11 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ ElementType.METHOD, ElementType.TYPE })
+/**
+ * Mark aspect's annotations for usage check
+ * @author mlaroche
+ */
+@Target({ ElementType.ANNOTATION_TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AspectAnnotation {
 	//
