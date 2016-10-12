3429113797a8aee84d8b3efc6a7280dba79d0fc0
NPi2Loup <npiedeloup@gmail.com>
2016-10-12 17:45:07 +0200

    updated pomFull

---
+++ gitCheckOut.bat
@@ -0,0 +1,29 @@
+cd ..\vertigo\
+rem git clean -f
+rem git checkout .
+
+cd ..\vertigo-extensions\
+git clean -f
+git checkout .
+
+cd ..\vertigo-quarto\
+git clean -f
+git checkout .
+
+cd ..\vertigo-shell\
+git clean -f
+git checkout .
+
+cd ..\vertigo-stella\
+git clean -f
+git checkout .
+
+cd ..\vertigo-struts2\
+git clean -f
+git checkout .
+
+cd ..\vertigo-studio\
+git clean -f
+git checkout .
+
+cd ..\vertigo\
