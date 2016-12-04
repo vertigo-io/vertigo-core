ec0a13a906beb59abd0ede75431ddab4ad1c6582
phlppchrtn <pchretien55@gmail.com>
2016-12-04 21:33:44 +0100

    [core] added final, public and assertions

--- vertigo-core/src/main/java/io/vertigo/util/Selector.java
+++ vertigo-core/src/main/java/io/vertigo/util/Selector.java
@@ -19,19 +19,29 @@ import io.vertigo.lang.Tuples.Tuple2;
 
 /**
  * Selector of classes and methods.
+ * 
+ * 1. Define  a scope/set of classes 
+ *  - by addind them
+ *  - by their packages
+ *  
+ * 2. filter 
+ *  - classes
+ *  - methods
+ * 
+ * 3 - find your classes or methods.
+ * 
  * @author mlaroche
  *
  */
-public class Selector {
-
+public final class Selector {
 	private final Map<String, Class> scope = new HashMap<>();
 
 	private final List<MethodConditions> methodConditions = new ArrayList<>();
 	private final List<ClassConditions> classConditions = new ArrayList<>();
 
-	private boolean scoped = false;
+	private boolean scoped;
 
-	Selector add(final Class clazz) {
+	public Selector add(final Class clazz) {
 		Assertion.checkNotNull(clazz);
 		Assertion.checkState(!scoped, "Classes cannot be added to scope after filtering");
 		// ---
@@ -39,7 +49,7 @@ public class Selector {
 		return this;
 	}
 
-	Selector addAll(final Collection<Class> classes) {
+	public Selector addAll(final Collection<Class> classes) {
 		Assertion.checkNotNull(classes);
 		Assertion.checkState(!scoped, "Classes cannot be added to scope after filtering");
 		// ---
@@ -47,7 +57,7 @@ public class Selector {
 		return this;
 	}
 
-	Selector includeTree(final String packageName) {
+	public Selector includeTree(final String packageName) {
 		Assertion.checkArgNotEmpty(packageName);
 		Assertion.checkState(!scoped, "Classes cannot be added to scope after filtering");
 		// ---
@@ -55,21 +65,21 @@ public class Selector {
 		return this;
 	}
 
-	Selector filter(final MethodConditions methodCondition) {
+	public Selector filter(final MethodConditions methodCondition) {
 		scoped = true;
 		// ---
 		methodConditions.add(methodCondition);
 		return this;
 	}
 
-	Selector filter(final ClassConditions classCondition) {
+	public Selector filter(final ClassConditions classCondition) {
 		scoped = true;
 		// ---
 		classConditions.add(classCondition);
 		return this;
 	}
 
-	Collection<Class> find() {
+	public Collection<Class> find() {
 		return Stream.concat(findClasses().stream(), findMethods().stream()
 				.map((final Tuple2<Class, Method> tuple) -> tuple.getVal1()))
 				.distinct()
@@ -77,7 +87,7 @@ public class Selector {
 
 	}
 
-	Collection<Class> findClasses() {
+	public Collection<Class> findClasses() {
 		final Predicate<Class> allPredicate = classConditions.stream()
 				.map(classCondition -> classCondition.getPredicate())
 				.reduce((method) -> true, (predicate1, predicate2) -> predicate1.and(predicate2));
@@ -87,7 +97,7 @@ public class Selector {
 				.collect(Collectors.toList());
 	}
 
-	Collection<Tuple2<Class, Method>> findMethods() {
+	public Collection<Tuple2<Class, Method>> findMethods() {
 		final Predicate<Method> allMethodPredicate = methodConditions.stream()
 				.map(classCondition -> classCondition.getPredicate())
 				.reduce((method) -> true, (predicate1, predicate2) -> predicate1.and(predicate2));
@@ -104,53 +114,65 @@ public class Selector {
 	 * @author mlaroche
 	 *
 	 */
-	public static class MethodConditions {
-
+	public static final class MethodConditions {
 		private final Predicate<Method> predicate;
 
 		private MethodConditions(final Predicate<Method> predicate) {
+			Assertion.checkNotNull(predicate);
+			//---
 			this.predicate = predicate;
 		}
 
 		Predicate<Method> getPredicate() {
 			return predicate;
 		}
-
-		static MethodConditions annotatedWith(final Class<? extends Annotation> annotationClass) {
+		
+		public static MethodConditions annotatedWith(final Class<? extends Annotation> annotationClass) {
+			Assertion.checkNotNull(annotationClass);
+			//---
 			return new MethodConditions((method) -> method.getAnnotationsByType(annotationClass).length > 0);
 		}
 
-		MethodConditions or(final MethodConditions methodCondition) {
+		public MethodConditions or(final MethodConditions methodCondition) {
+			Assertion.checkNotNull(methodCondition);
+			//---
 			return new MethodConditions(methodCondition.getPredicate().or(predicate));
 		}
 	}
 
 	/**
-	 * Condition for selecting a class.
+	 * Conditions for selecting a class.
 	 * @author mlaroche
 	 *
 	 */
-	public static class ClassConditions {
-
+	public static final class ClassConditions {
 		private final Predicate<Class> predicate;
 
 		private ClassConditions(final Predicate<Class> predicate) {
+			Assertion.checkNotNull(predicate);
+			//---
 			this.predicate = predicate;
 		}
 
 		Predicate<Class> getPredicate() {
 			return predicate;
 		}
-
-		static ClassConditions annotatedWith(final Class<? extends Annotation> annotationClass) {
+		
+		public static ClassConditions annotatedWith(final Class<? extends Annotation> annotationClass) {
+			Assertion.checkNotNull(annotationClass);
+			//---
 			return new ClassConditions((clazz) -> clazz.getAnnotationsByType(annotationClass).length > 0);
 		}
 
-		static ClassConditions subTypeOf(final Class clazz) {
+		public static ClassConditions subTypeOf(final Class clazz) {
+			Assertion.checkNotNull(clazz);
+			//---
 			return new ClassConditions((subtype) -> clazz.isAssignableFrom(subtype));
 		}
 
-		ClassConditions or(final ClassConditions classCondition) {
+		public ClassConditions or(final ClassConditions classCondition) {
+			Assertion.checkNotNull(classCondition);
+			//---
 			return new ClassConditions(classCondition.getPredicate().or(predicate));
 		}
 	}
