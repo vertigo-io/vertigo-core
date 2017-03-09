121cfee12f1ade4405c0fe7b66e14b1d0f99aa78
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2017-03-09 19:46:26 +0100

    [commons] lazy + queue

--- vertigo-commons-impl/src/main/java/io/vertigo/commons/plugins/analytics/log/SocketLoggerAnalyticsConnectorPlugin.java
+++ vertigo-commons-impl/src/main/java/io/vertigo/commons/plugins/analytics/log/SocketLoggerAnalyticsConnectorPlugin.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Collections;
 import java.util.Optional;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -15,6 +16,7 @@ import org.apache.log4j.net.SocketAppender;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import io.vertigo.commons.daemon.DaemonManager;
 import io.vertigo.commons.impl.analytics.AProcess;
 import io.vertigo.commons.impl.analytics.AnalyticsConnectorPlugin;
 import io.vertigo.lang.Assertion;
@@ -27,39 +29,39 @@ public final class SocketLoggerAnalyticsConnectorPlugin implements AnalyticsConn
 	private static final Gson GSON = new GsonBuilder().create();
 	private static final int DEFAULT_SERVER_PORT = 4560;// DefaultPort of SocketAppender
 
-	private final Logger socketLogger;
+	private Logger socketLogger;
 	private final String nodeIdentifier;
+	private final String hostName;
+	private final int port;
+
+	private final ConcurrentLinkedQueue<AProcess> processQueue = new ConcurrentLinkedQueue<>();
 
 	/**
-	 * Contructor
+	 * Constructor.
 	 * @param hostNameOpt hostName of the remote server
 	 * @param portOpt port of the remote server
 	 */
 	@Inject
-	public SocketLoggerAnalyticsConnectorPlugin(@Named("appName") final String appName,
+	public SocketLoggerAnalyticsConnectorPlugin(
+			final DaemonManager daemonManager,
+			@Named("appName") final String appName,
 			@Named("hostName") final Optional<String> hostNameOpt,
 			@Named("port") final Optional<Integer> portOpt) {
 		Assertion.checkArgNotEmpty(appName);
 		Assertion.checkNotNull(hostNameOpt);
 		Assertion.checkNotNull(portOpt);
 		// ---
-		final String hostName = hostNameOpt.orElse("analytica.part.klee.lan.net");
-		final int port = portOpt.orElse(DEFAULT_SERVER_PORT);
+		hostName = hostNameOpt.orElse("analytica.part.klee.lan.net");
+		port = portOpt.orElse(DEFAULT_SERVER_PORT);
 		nodeIdentifier = appName + ":" + retrieveHostName();
 		//---
-		socketLogger = createLogger(hostName, port);
-
+		daemonManager.registerDaemon("remoteLogger", () -> this::pollQueue, 1);
 	}
 
 	/** {@inheritDoc} */
 	@Override
 	public void add(final AProcess process) {
-		if (socketLogger.isInfoEnabled()) {
-			final StringBuilder log = new StringBuilder(nodeIdentifier)
-					.append(" - ")
-					.append(GSON.toJson(Collections.singletonList(process)));
-			socketLogger.info(log.toString());
-		}
+		processQueue.add(process);
 	}
 
 	private static String retrieveHostName() {
@@ -82,6 +84,28 @@ public final class SocketLoggerAnalyticsConnectorPlugin implements AnalyticsConn
 		logger.addAppender(appender);
 		logger.setLevel(Level.INFO);
 		return logger;
+	}
+
+	private void pollQueue() {
+		while (!processQueue.isEmpty()) {
+			final AProcess head = processQueue.poll();
+			if (head != null) {
+				sendProcess(head);
+			}
+		}
+
+	}
+
+	private void sendProcess(final AProcess process) {
+		if (socketLogger == null) {
+			socketLogger = createLogger(hostName, port);
+		}
+		if (socketLogger.isInfoEnabled()) {
+			final StringBuilder log = new StringBuilder(nodeIdentifier)
+					.append(" - ")
+					.append(GSON.toJson(Collections.singletonList(process)));
+			socketLogger.info(log.toString());
+		}
 	}
 
 }
