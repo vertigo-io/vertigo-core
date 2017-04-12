0a10feae856b4bfe94594fd20b58caf792a306c8
Piedeloup Nicolas <npi2loup@gmail.com>
2017-04-12 17:51:42 +0200

    [Core] Better exception if a looking for class don't extends the class

--- vertigo-core/src/main/java/io/vertigo/util/ClassUtil.java
+++ vertigo-core/src/main/java/io/vertigo/util/ClassUtil.java
@@ -174,6 +174,8 @@ public final class ClassUtil {
 			return Class.forName(javaClassName).asSubclass(type);
 		} catch (final ClassNotFoundException e) {
 			throw WrappedException.wrap(e, "Impossible de trouver la classe : " + javaClassName);
+		} catch (final ClassCastException e) {
+			throw WrappedException.wrap(e, "La classe " + javaClassName + " doit être une sous-class de : " + type.getSimpleName());
 		}
 	}
 
