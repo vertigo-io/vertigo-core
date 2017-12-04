4098b0fa4fb4533f5c0faeaf12cb9836600c280d
Piedeloup Nicolas <npi2loup@gmail.com>
2017-12-04 18:56:44 +0100

    [Commons] Fixed Instant GsonAdapter

--- vertigo-commons-impl/src/main/java/io/vertigo/commons/plugins/node/infos/http/HttpNodeInfosPlugin.java
+++ vertigo-commons-impl/src/main/java/io/vertigo/commons/plugins/node/infos/http/HttpNodeInfosPlugin.java
@@ -24,12 +24,20 @@ import java.io.InputStream;
 import java.lang.reflect.Type;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.time.Instant;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.google.gson.reflect.TypeToken;
 
 import io.vertigo.commons.analytics.health.HealthCheck;
@@ -45,7 +53,24 @@ import io.vertigo.lang.WrappedException;
  */
 public final class HttpNodeInfosPlugin implements NodeInfosPlugin {
 
-	private static final Gson GSON = new Gson();
+	private static class InstantAdapter implements JsonSerializer<Instant>, JsonDeserializer<Instant> {
+
+		/** {@inheritDoc} */
+		@Override
+		public JsonElement serialize(final Instant date, final Type typeOfSrc, final JsonSerializationContext context) {
+			return new JsonPrimitive(date.toString()); // "yyyy-mm-ddTHH:MI:SSZ"
+		}
+
+		/** {@inheritDoc} */
+		@Override
+		public Instant deserialize(final JsonElement jsonElement, final Type type, final JsonDeserializationContext jsonDeserializationContext) {
+			return Instant.parse(jsonElement.getAsString());
+		}
+	}
+
+	private static final Gson GSON = new GsonBuilder()
+			.registerTypeAdapter(Instant.class, new InstantAdapter())
+			.create();
 
 	@Override
 	public String getConfig(final Node app) {
