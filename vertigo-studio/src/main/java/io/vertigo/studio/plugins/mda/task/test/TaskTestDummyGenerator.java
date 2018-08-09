20b8eb62f57f03c0bc18ff9b73e2f0dfbbe748e7
Piedeloup Nicolas <npi2loup@gmail.com>
2018-08-09 10:30:34 +0200

    [Studio] Fixed javadoc

--- vertigo-studio/src/main/java/io/vertigo/studio/plugins/mda/task/test/TaskTestDummyGenerator.java
+++ vertigo-studio/src/main/java/io/vertigo/studio/plugins/mda/task/test/TaskTestDummyGenerator.java
@@ -16,13 +16,15 @@ public interface TaskTestDummyGenerator {
 	/**
 	 * Creates a dummy value for the specified type
 	 * @param type class of the wanted object
+	 * @param <T> class of the wanted object
 	 * @return dummy value
 	 */
 	public <T> T dum(final Class<T> type);
 
 	/**
 	 * Creates a list of dummy values for the specified type
-	 * @param type class of the wanted object
+	 * @param clazz class of the wanted object
+	 * @param <T> class of the wanted object
 	 * @return dummy values as List
 	 */
 	public <T> List<T> dumList(final Class<T> clazz);
@@ -30,6 +32,7 @@ public interface TaskTestDummyGenerator {
 	/**
 	 * Creates a dtList of dummy values for the specified type
 	 * @param dtoClass class of the wanted object
+	 * @param <D> class of the wanted object
 	 * @return dummy values as DtList
 	 */
 	public <D extends DtObject> DtList<D> dumDtList(final Class<D> dtoClass);
@@ -37,6 +40,7 @@ public interface TaskTestDummyGenerator {
 	/**
 	 * Creates a dummy dtObject for the specified type as new (no pk)
 	 * @param dtoClass class of the wanted object
+	 * @param <D> class of the wanted object
 	 * @return dummy value
 	 */
 	public <D extends DtObject> D dumNew(final Class<D> dtoClass);
