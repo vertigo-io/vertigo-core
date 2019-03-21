b52b96dba21e41be5c78a1f5643e8df5975c6a64
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-03-21 18:00:20 +0100

    [all] licence + version

--- vertigo-core/src/main/java/io/vertigo/app/Logo.java
+++ vertigo-core/src/main/java/io/vertigo/app/Logo.java
@@ -1,7 +1,7 @@
 /**
  * vertigo - simple java starter
  *
- * Copyright (C) 2013-2018, KleeGroup, direction.technique@kleegroup.com (http://www.kleegroup.com)
+ * Copyright (C) 2013-2019, KleeGroup, direction.technique@kleegroup.com (http://www.kleegroup.com)
  * KleeGroup, Centre d'affaire la Boursidiere - BP 159 - 92357 Le Plessis Robinson Cedex - France
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -39,7 +39,7 @@ final class Logo {
 		out.println("+--------------------------------------------------+");
 		out.println("|   _____________                                  |");
 		out.println("|  |     _     / |                                 |");
-		out.println("|  |#   / \\   / /|  Vertigo v1.1.2 - 2018          |");
+		out.println("|  |#   / \\   / /|  Vertigo v1.1.3 - 2019          |");
 		out.println("|  |  __\\ /__/ / |                                 |");
 		out.println("|  | / _      /  |                                 |");
 		out.println("|  |/ / \\  ()/  *|                                 |");
