1c85698d8b06caf2a3ada19fc334193c7cebec57
mlaroche <matthieu.laroche@telecom-bretagne.eu>
2019-05-09 09:04:40 +0200

    [core] added check

--- vertigo-core/src/main/java/io/vertigo/core/component/ComponentSpace.java
+++ vertigo-core/src/main/java/io/vertigo/core/component/ComponentSpace.java
@@ -44,12 +44,13 @@ public interface ComponentSpace extends Container {
 	/**
 	 * Creates an new object instance of the given class and inject dependencies using the current App ComponentSpace as container.
 	 * This created object is not registered in the ComponantSpace.
-	 * Therefore the clazz cannot implement the interface Activeable or DefinitionProvider because the lifecycle of this component is not handled by Vertigo.
+	 * Therefore the clazz cannot implement the interface Activeable because the lifecycle of this component is not handled by Vertigo.
 	 * @param clazz the clazz of the object your want to create with it's member injected.
 	 * @return the newly created object.
 	 */
 	public static <T> T newInstance(final Class<T> clazz) {
 		Assertion.checkNotNull(clazz);
+		Assertion.checkState(!clazz.isAssignableFrom(Activeable.class), " {0} is an Activeable component and must be registred in the NodeConfig for creation at the application startup", clazz);
 		// ---
 		return ComponentSpaceLoader.createInstance(clazz, Home.getApp().getComponentSpace(), Optional.empty(), Collections.emptyMap());
 	}
