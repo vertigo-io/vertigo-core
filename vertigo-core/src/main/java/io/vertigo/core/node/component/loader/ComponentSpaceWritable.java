075577958b6d55ea3359ee5cebec9da9f9b36da6
phlppchrtn <philippe.chretien@kleegroup.com>
2021-05-31 23:07:31 +0200

    Java 16

--- vertigo-core/src/main/java/io/vertigo/core/node/component/loader/ComponentSpaceWritable.java
+++ vertigo-core/src/main/java/io/vertigo/core/node/component/loader/ComponentSpaceWritable.java
@@ -120,14 +120,14 @@ public final class ComponentSpaceWritable implements ComponentSpace, Activeable 
 	}
 
 	private static void startComponent(final CoreComponent component) {
-		if (component instanceof Activeable) {
-			Activeable.class.cast(component).start();
+		if (component instanceof Activeable activeable) {
+			activeable.start();
 		}
 	}
 
 	private static void stopComponent(final CoreComponent component) {
-		if (component instanceof Activeable) {
-			Activeable.class.cast(component).stop();
+		if (component instanceof Activeable activeable) {
+			activeable.stop();
 		}
 	}
 
