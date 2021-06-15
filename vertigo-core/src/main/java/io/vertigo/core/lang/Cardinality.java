9e649512bb409db410e75d76e5d6fd639b90782d
phlppchrtn <philippe.chretien@kleegroup.com>
2021-06-15 08:47:55 +0200

    new switch

--- vertigo-core/src/main/java/io/vertigo/core/lang/Cardinality.java
+++ vertigo-core/src/main/java/io/vertigo/core/lang/Cardinality.java
@@ -50,29 +50,21 @@ public enum Cardinality {
 	public static Cardinality fromSymbol(final String sCardinality) {
 		Assertion.check().isNotBlank(sCardinality);
 		//---
-		switch (sCardinality) {
-			case "?":
-				return OPTIONAL_OR_NULLABLE;
-			case "1":
-				return ONE;
-			case "*":
-				return MANY;
-			default:
-				throw new VSystemException("Unknown cardinality symbol : '{0}'. Supported cardinalities are '?' for optional, '1' for one and '*' for many ", sCardinality);
-		}
+		return switch (sCardinality) {
+			case "?" -> OPTIONAL_OR_NULLABLE;
+			case "1" -> ONE;
+			case "*" -> MANY;
+			default -> throw new VSystemException("Unknown cardinality symbol : '{0}'. Supported cardinalities are '?' for optional, '1' for one and '*' for many ", sCardinality);
+		};
 	}
 
 	public String toSymbol() {
-		switch (this) {
-			case OPTIONAL_OR_NULLABLE:
-				return "?";
-			case ONE:
-				return "1";
-			case MANY:
-				return "*";
-			default:
-				throw new VSystemException("Unknown cardinality : '{0}'. Supported cardinalities are optional, one and many ", this);
-		}
+		return switch (this) {
+			case OPTIONAL_OR_NULLABLE -> "?";
+			case ONE -> "1";
+			case MANY -> "*";
+			default -> throw new VSystemException("Unknown cardinality : '{0}'. Supported cardinalities are optional, one and many ", this);
+		};
 	}
 
 }
