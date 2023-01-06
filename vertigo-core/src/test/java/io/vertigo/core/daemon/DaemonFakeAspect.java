861643b7323aad562c127b3d7e8231695dba5c6d
Matthieu Laroche <matthieu.laroche@telecom-bretagne.eu>
2023-01-06 15:12:23 +0100

    update date in license and version

--- vertigo-core/src/test/java/io/vertigo/core/daemon/DaemonFakeAspect.java
+++ vertigo-core/src/test/java/io/vertigo/core/daemon/DaemonFakeAspect.java
@@ -1,7 +1,7 @@
 /**
  * vertigo - application development platform
  *
- * Copyright (C) 2013-2022, Vertigo.io, team@vertigo.io
+ * Copyright (C) 2013-2023, Vertigo.io, team@vertigo.io
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
