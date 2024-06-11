c59cabfa9a366129ee27c63d2976c4f005b3ab12
phlppchrtn <philippe.chretien@kleegroup.com>
2024-06-12 00:02:20 +0200

    Javadoc

--- vertigo-core/src/main/java/io/vertigo/core/locale/LocaleMessageText.java
+++ vertigo-core/src/main/java/io/vertigo/core/locale/LocaleMessageText.java
@@ -27,28 +27,28 @@ import io.vertigo.core.node.Node;
 import io.vertigo.core.util.StringUtil;
 
 /**
- * Texte pouvant être externalisé dans un fichier de ressources,
- * en fonction du paramétrage de l'application.
- * Si le libelle n'est pas trouvé, l'affichage est
+ * A Text that can be externalized in a resource file,
+ * based on the application's configuration settings.
+ * If the label is not found, the display is
  *
  * @author npiedeloup, pchretien
  */
 public final class LocaleMessageText implements Serializable {
 	private static final long serialVersionUID = 4723023230514051954L;
-	/** Clé du libellé dans le dictionnaire. */
+	/** Key of the label in the dictionary. */
 	private final LocaleMessageKey key;
-	/** Libellé non formatté. */
+	/** Unformatted label. */
 	private final String defaultMsg;
-	/** paramètres permettant de formatter le libellé. */
+	/** Parameters allowing to format the label. */
 	private final Serializable[] params;
 
 	/**
 	 * Constructor.
-	 * La clé et/ou le message par défaut doit être non null.
+	 * The key and/or default message must be non-null.
 	 *
-	 * @param defaultMsg Message par défaut (non formatté) de la ressource
-	 * @param key Clé de la ressource
-	 * @param params paramètres de la ressource
+	 * @param defaultMsg Default message (unformatted) of the resource
+	 * @param key Key of the resource
+	 * @param params Parameters of the resource
 	 */
 	private LocaleMessageText(final String defaultMsg, final LocaleMessageKey key, final Serializable... params) {
 		Assertion.check()
@@ -61,9 +61,9 @@ public final class LocaleMessageText implements Serializable {
 	}
 
 	/**
-	 * static Builder of a messageText by its key.
+	 * Static Builder of a messageText by its key.
 	 *
-	 * @param key Clé de la ressource
+	 * @param key Key of the resource
 	 * @return the messageText
 	 */
 	public static LocaleMessageText of(final LocaleMessageKey key, final Serializable... params) {
@@ -73,9 +73,9 @@ public final class LocaleMessageText implements Serializable {
 	}
 
 	/**
-	 * static Builder of a messageText by its default message.
+	 * Static Builder of a messageText by its default message.
 	 *
-	 * @param msg Message par défaut (non formatté) de la ressource
+	 * @param msg Default message (unformatted) of the resource
 	 * @return the messageText
 	 */
 	public static LocaleMessageText of(final String msg, final Serializable... params) {
@@ -85,9 +85,9 @@ public final class LocaleMessageText implements Serializable {
 	}
 
 	/**
-	 * static Builder of a messageText by its default message.
+	 * Static Builder of a messageText by its default message.
 	 *
-	 * @param defaultMsg Message par défaut (non formatté) de la ressource
+	 * @param defaultMsg Default message (unformatted) of the resource
 	 * @return the messageText
 	 */
 	public static LocaleMessageText ofDefaultMsg(final String defaultMsg, final LocaleMessageKey key, final Serializable... params) {
@@ -99,7 +99,7 @@ public final class LocaleMessageText implements Serializable {
 	}
 
 	/**
-	 * @return paramètres du message
+	 * @return parameters of the message
 	 */
 	private Object[] getParams() {
 		return params;
@@ -107,7 +107,7 @@ public final class LocaleMessageText implements Serializable {
 
 	/**
 	 * Format message with parameters.
-	 * No exception throwed !!
+	 * No exception thrown!!
 	 *
 	 * @return Formatted message, if exists.
 	 */
@@ -115,24 +115,25 @@ public final class LocaleMessageText implements Serializable {
 		Locale locale = null;
 		String msg = null;
 		if (key != null) {
-			//On ne recherche le dictionnaire (géré par localeManager) que si il y a une clé.
+			// Only search the dictionary (managed by localeManager) if there is a key.
 			try {
-				//Il est nécessaire que LocaleManager soit enregistré.
-				//Si pas d'utilisateur on prend la première langue déclarée.
+				// It is necessary that LocaleManager is registered.
+				// If no user is present, the first declared language is taken.
 				final var localeManager = getLocaleManager();
 				locale = localeManager.getCurrentLocale();
 				msg = localeManager.getMessage(key, locale);
 			} catch (final Exception e) {
-				//Si pas de locale msg est null et on va récupérer s'il existe le message par défaut.
+				// If LocaleManager is not registered or generates an exception
+				// then msg remains null and we retrieve the default message if it exists.
 			}
 		}
 
-		//Si pas de clé on recherche le libellé par défaut.
+		// If no key is found, we search for the default message.
 		if (msg == null) {
 			msg = defaultMsg;
 		}
 		if (msg != null) {
-			//On passe toujours dans le StringUtil.format pour unifier.
+			// We always pass through StringUtil.format to unify.
 			return Optional.of(StringUtil.format(msg, getParams()));
 		}
 
@@ -141,16 +142,16 @@ public final class LocaleMessageText implements Serializable {
 
 	/**
 	 * Format message with parameters.
-	 * If nothing found, return a "panic message" displaying what is missing.
-	 * No exception throwed !!
+	 * If nothing is found, return a "panic message" displaying what is missing.
+	 * No exception thrown!!
 	 *
 	 * @return Formatted message.
 	 */
 	public String getDisplay() {
 		/*
-		 * Cette méthode doit toujours remonter un message.
-		 * Si LocaleManager n'est pas enregistré ou génère une exception
-		 * alors on se contente de retourner la clé du message.
+		 * This method must always return a message.
+		 * If LocaleManager is not registered or generates an exception
+		 * then we return the key of the message.
 		 */
 		return getDisplayOpt().orElseGet(() -> getPanicMessage());
 	}
