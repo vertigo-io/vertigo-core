027431528c430d58980504722fd39834d523566e
skerdudou <skerdudou@users.noreply.github.com>
2026-02-18 15:52:16 +0100

    Fix : FileUtil:checkUserPath : invalid \0 character detection

--- vertigo-core/src/main/java/io/vertigo/core/util/FileUtil.java
+++ vertigo-core/src/main/java/io/vertigo/core/util/FileUtil.java
@@ -197,7 +197,7 @@ public final class FileUtil {
 	 * @param userPath Path to check
 	 */
 	public static void checkUserPath(final String userPath) {
-		Assertion.check().isFalse(userPath.contains("..")
+		Assertion.check().isTrue(!userPath.contains("..")
 				&& userPath.indexOf((char) 0) == -1, //char 0
 				USER_CHECK_ERROR_MSG);
 	}
