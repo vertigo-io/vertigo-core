f425d51d04d8ad2aa698128ad783a9c7e32ec660
skerdudou <skerdudou@users.noreply.github.com>
2026-08-11 17:28:23 +0200

    Merge v4-v5 follow-up: port v4 version bumps and vertigoCI profile
    - gson 2.14.0 (required by SerializableParameterizedType), snakeyaml 2.6, log4j 2.26.0
    - vertigo-parent: port v4 maven plugin upgrades, rename jenkins profile to vertigoCI (dependency-check 12.2.2 autoUpdate=false, jacoco 0.8.14), fix scm tag to 5.0.0-SNAPSHOT
    - Tuple.of: remove non-null assertions (keep v4 behavior)

--- vertigo-core/src/main/java/io/vertigo/core/lang/Tuple.java
+++ vertigo-core/src/main/java/io/vertigo/core/lang/Tuple.java
@@ -26,7 +26,7 @@ package io.vertigo.core.lang;
  * @param <A> Type of first object
  * @param <B> Type of second object
  */
-public record Tuple<A, B> (A val1, B val2) {
+public record Tuple<A, B>(A val1, B val2) {
 
 	/**
 	 * Creates a Tuple with two objects.
@@ -38,10 +38,6 @@ public record Tuple<A, B> (A val1, B val2) {
 	 * @return New Tuple
 	 */
 	public static <A, B> Tuple<A, B> of(final A val1, final B val2) {
-		Assertion.check()
-				.isNotNull(val1)
-				.isNotNull(val2);
-		//---
 		return new Tuple<>(val1, val2);
 	}
 }
